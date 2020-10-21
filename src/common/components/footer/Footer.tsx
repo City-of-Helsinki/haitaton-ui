822a0cd865a1e64ea158541f9e28f06e42d6155b
Valtteri Laukkanen <1valtteri.laukkanen@gmail.com>
2020-10-21 15:35:50 +0300

    footer fix

--- src/common/components/footer/Footer.tsx
+++ src/common/components/footer/Footer.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Koros } from 'hds-react';
 import './styles.scss';
 
 const FooterComp: React.FC = () => {
-  return <footer className="footer">&copy; Copyright 2020 &#8226; All rights reserved</footer>;
+  return (
+    <footer className="footer">
+      <Koros type="basic" />
+      <p>&copy; Copyright 2020 &#8226; All rights reserved</p>
+    </footer>
+  );
 };
 
 export default FooterComp;
