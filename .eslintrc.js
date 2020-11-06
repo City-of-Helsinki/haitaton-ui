2bc902ecf160f5f2731c0d46483e6722a73cac5a
Petteri Innanmaa <petteri.innanmaa@gofore.com>
2020-11-06 13:43:59 +0200

    Change eslint endOfLine rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -69,6 +69,12 @@ module.exports = {
         ignorePropertyModificationsFor: ['state'],
       },
     ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
   settings: {
     react: {
