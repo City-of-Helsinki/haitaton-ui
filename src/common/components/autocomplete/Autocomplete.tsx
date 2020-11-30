045e21fdaf024b297c4ee751097f3dcd4375c959
Petteri Innanmaa <petteri.innanmaa@gofore.com>
2020-11-30 12:20:54 +0200

    HAI-173: Organisaatio autocomplete

--- src/common/components/autocomplete/Autocomplete.tsx
+++ src/common/components/autocomplete/Autocomplete.tsx
@@ -1,64 +1,48 @@
 import React from 'react';
-import { Controller, Control } from 'react-hook-form';
 import { Combobox, Tooltip, TooltipProps } from 'hds-react';
 
-type Option = { value: string | number; label: string };
+export type Option = { value: string | number; label: string };
+
+type Options = Array<Option>;
 
 type PropTypes = {
-  name: string;
-  id: string;
-  control: Control;
-  rules?: { required: boolean };
-  defaultValue: string;
+  defaultValue: Partial<Option>;
   label: string;
-  options: Array<Option>;
+  options: Options;
   invalid?: boolean;
   errorMsg?: string;
   tooltip?: TooltipProps;
+  onChange: (value: Option) => void;
+  className: string;
 };
 
+const findSelected = (options: Options, defaultValue?: Partial<Option>) =>
+  defaultValue
+    ? options.find((o) => o.value === defaultValue.value || o.label === defaultValue.label)
+    : undefined;
+
 const Autocomplete: React.FC<PropTypes> = ({
-  name,
-  id,
-  control,
-  rules,
   options,
   defaultValue,
   label,
   invalid,
   errorMsg,
+  onChange,
   tooltip,
-}) => {
-  return (
-    <div className="autocomplete">
-      {!!tooltip && <Tooltip {...tooltip} />}
-      <Controller
-        name={name}
-        id={id}
-        control={control}
-        defaultValue={defaultValue}
-        rules={rules}
-        render={({ onChange, value }) => {
-          return (
-            <Combobox
-              label={label}
-              // helper="Assistive text"
-              placeholder="Placeholder"
-              // toggleButtonAriaLabel="Toggle menu"
-              options={options}
-              defaultValue={
-                defaultValue ? options.find((o) => o.value === defaultValue) : undefined
-              }
-              value={options.find((o) => o.value === value)}
-              // eslint-disable-next-line
-              onChange={(option: any) => onChange(option.value)}
-            />
-          );
-        }}
-      />
-      {invalid && <span className="error-text">{errorMsg}</span>}
-    </div>
-  );
-};
+  ...rest
+}) => (
+  <div className="autocomplete">
+    {!!tooltip && <Tooltip {...tooltip} />}
+    <Combobox
+      label={label}
+      options={options}
+      defaultValue={findSelected(options, defaultValue)}
+      // value={findSelected(options, defaultValue)}
+      onChange={(option: Option) => onChange(option)}
+      {...rest}
+    />
+    {invalid && <span className="error-text">{errorMsg}</span>}
+  </div>
+);
 
 export default Autocomplete;
--- src/domain/hanke/edit/Form2.tsx
+++ src/domain/hanke/edit/Form2.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
-import { $enum } from 'ts-enum-util';
+import React, { useEffect } from 'react';
+import { useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { useTypedController } from '@hookform/strictly-typed';
 import { TextInput } from 'hds-react';
 import { FormProps, FORMFIELD, CONTACT_FORMFIELD, HankeDataDraft } from './types';
 import H2 from '../../../common/components/text/H2';
 import H3 from '../../../common/components/text/H3';
+import Autocomplete, { Option } from '../../../common/components/autocomplete/Autocomplete';
 
 const CONTACT_TYPES = [FORMFIELD.OMISTAJAT, FORMFIELD.ARVIOIJAT, FORMFIELD.TOTEUTTAJAT];
+const CONTACT_FIELDS = [
+  CONTACT_FORMFIELD.ETUNIMI,
+  CONTACT_FORMFIELD.SUKUNIMI,
+  CONTACT_FORMFIELD.EMAIL,
+  CONTACT_FORMFIELD.PUHELINNUMERO,
+  CONTACT_FORMFIELD.OSASTO,
+];
 
-const Form2: React.FC<FormProps> = ({ control, formData }) => {
+const organisaatioOptions = [
+  { label: 'Organisaatio 1', value: 1 },
+  { label: 'Organisaatio 2', value: 2 },
+];
+
+const Form2: React.FC<FormProps> = ({ control, formData, register }) => {
   const { t } = useTranslation();
+  const { setValue } = useFormContext();
   const TypedController = useTypedController<HankeDataDraft>({ control });
+
+  // Autocomplete doesnt register fields so we need manually register them
+  useEffect(() => {
+    CONTACT_TYPES.forEach((contactType) => {
+      register({ name: `${contactType}[0].organisaatioNimi`, type: 'custom' }, { required: false });
+      register({ name: `${contactType}[0].organisaatioId`, type: 'custom' }, { required: false });
+    });
+  }, []);
 
   return (
     <div className="form2">
@@ -20,29 +42,46 @@ const Form2: React.FC<FormProps> = ({ control, formData }) => {
         <div key={CONTACT_TYPE}>
           <H3>{t(`hankeForm:headers:${CONTACT_TYPE}`)}</H3>
           <div className="formColumns">
-            {$enum(CONTACT_FORMFIELD)
-              .getValues()
-              .map((contactField) => (
-                <TypedController
-                  key={contactField}
+            {CONTACT_FIELDS.map((contactField) => (
+              <TypedController
+                key={contactField}
+                // eslint-disable-next-line
+                // @ts-ignore
+                name={[CONTACT_TYPE, 0, contactField]}
+                defaultValue={
                   // eslint-disable-next-line
                   // @ts-ignore
-                  name={[CONTACT_TYPE, 0, contactField]}
-                  defaultValue={
-                    // eslint-disable-next-line
-                    // @ts-ignore
-                    formData[CONTACT_TYPE] ? formData[CONTACT_TYPE][0][contactField] : ''
-                  }
-                  render={(formProps) => (
-                    <TextInput
-                      className="formItem"
-                      id={`${CONTACT_TYPE}-${contactField}`}
-                      {...formProps}
-                      label={t(`hankeForm:labels:${contactField}`)}
-                    />
-                  )}
-                />
-              ))}
+                  formData[CONTACT_TYPE] ? formData[CONTACT_TYPE][0][contactField] : ''
+                }
+                render={(formProps) => (
+                  <TextInput
+                    className="formItem"
+                    id={`${CONTACT_TYPE}-${contactField}`}
+                    {...formProps}
+                    label={t(`hankeForm:labels:${contactField}`)}
+                  />
+                )}
+              />
+            ))}
+            <Autocomplete
+              className="formItem"
+              label={t(`hankeForm:labels:organisaatio`)}
+              options={organisaatioOptions}
+              // eslint-disable-next-line
+              // @ts-ignore
+              defaultValue={{
+                // eslint-disable-next-line
+                // @ts-ignore
+                label: formData[CONTACT_TYPE][0].organisaatioNimi,
+                // eslint-disable-next-line
+                // @ts-ignore
+                value: formData[CONTACT_TYPE][0].organisaatioId,
+              }}
+              onChange={(option: Option): void => {
+                setValue(`${CONTACT_TYPE}[0].organisaatioId`, option.value);
+                setValue(`${CONTACT_TYPE}[0].organisaatioNimi`, option.label);
+              }}
+            />
           </div>
         </div>
       ))}
