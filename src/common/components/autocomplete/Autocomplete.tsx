2ef940498ad7ac57864bde42cfef71773267cd75
Petteri Innanmaa <petteri.innanmaa@gofore.com>
2020-11-30 16:36:06 +0200

    HAI-173: Organizations autocomplete (#42)

---
+++ src/common/components/autocomplete/Autocomplete.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Combobox, Tooltip, TooltipProps } from 'hds-react';
+
+export type Option = { value: string | number; label: string };
+
+type Options = Array<Option>;
+
+type PropTypes = {
+  defaultValue: Partial<Option>;
+  label: string;
+  options: Options;
+  invalid?: boolean;
+  errorMsg?: string;
+  tooltip?: TooltipProps;
+  onChange: (value: Option) => void;
+  className: string;
+};
+
+const findSelected = (options: Options, defaultValue?: Partial<Option>) =>
+  defaultValue
+    ? options.find((o) => o.value === defaultValue.value || o.label === defaultValue.label)
+    : undefined;
+
+const Autocomplete: React.FC<PropTypes> = ({
+  options,
+  defaultValue,
+  label,
+  invalid,
+  errorMsg,
+  onChange,
+  tooltip,
+  ...rest
+}) => (
+  <div className="autocomplete">
+    {!!tooltip && <Tooltip {...tooltip} />}
+    <Combobox
+      label={label}
+      options={options}
+      defaultValue={findSelected(options, defaultValue)}
+      onChange={(option: Option) => onChange(option)}
+      {...rest}
+    />
+    {invalid && <span className="error-text">{errorMsg}</span>}
+  </div>
+);
+
+export default Autocomplete;
