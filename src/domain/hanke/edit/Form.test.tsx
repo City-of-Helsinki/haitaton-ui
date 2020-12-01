e398c66941120f91ba20c1bef40e86c16f7f2d0b
Petteri Innanmaa <petteri.innanmaa@gofore.com>
2020-12-01 15:44:52 +0200

    Fix tests

--- src/domain/hanke/edit/Form.test.tsx
+++ src/domain/hanke/edit/Form.test.tsx
@@ -5,7 +5,7 @@ import { HANKE_VAIHE, FORMFIELD } from './types';
 afterEach(cleanup);
 
 describe('Form', () => {
-  const obj1 = { [FORMFIELD.YKT_HANKE]: true };
+  const obj1 = { [FORMFIELD.YKT_HANKE]: true, [FORMFIELD.TUNNUS]: '', [FORMFIELD.NIMI]: '' };
   const obj2 = { [FORMFIELD.VAIHE]: HANKE_VAIHE.OHJELMOINTI };
   const result = {
     [FORMFIELD.YKT_HANKE]: true,
