6b4d527a9845b7482596da9be0ae0a4ee996f1fe
Valtteri Laukkanen <1valtteri.laukkanen@gmail.com>
2020-12-08 18:12:25 +0200

    E2E tests

--- src/domain/hanke/edit/FinishedForm.tsx
+++ src/domain/hanke/edit/FinishedForm.tsx
@@ -4,6 +4,6 @@ import H2 from '../../../common/components/text/H2';
 
 const FinishedForm: React.FC = () => {
   const { t } = useTranslation();
-  return <H2>{t('hankeForm:finishedForm:header')}</H2>;
+  return <H2 data-testid="finishedForm">{t('hankeForm:finishedForm:header')}</H2>;
 };
 export default FinishedForm;
