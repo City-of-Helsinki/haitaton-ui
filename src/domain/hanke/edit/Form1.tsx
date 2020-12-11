c886b8a019bfbdceeca9c8553735e38e0a38d4fa
Valtteri Laukkanen <1valtteri.laukkanen@gmail.com>
2020-12-11 14:20:02 +0200

    trying to fix tests

--- src/domain/hanke/edit/Form1.tsx
+++ src/domain/hanke/edit/Form1.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { FormProps } from './types';
+import HankeDrawer from '../../map/HankeDrawer';
+import { FormProps, FORMFIELD } from './types';
 import H2 from '../../../common/components/text/H2';
 
-const Form1: React.FC<FormProps> = () => {
+const Form1: React.FC<FormProps> = ({ formData }) => {
   const { t } = useTranslation();
   return (
     <div className="form1">
       <H2 data-testid="hankkeenAlue">{t('hankeForm:hankkeenAlueForm:header')}</H2>
+      <div style={{ position: 'relative' }}>
+        <HankeDrawer hankeTunnus={formData[FORMFIELD.TUNNUS]} />
+      </div>
     </div>
   );
 };
