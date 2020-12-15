7d89050f37adae50b6e0ed1bee5bf881be43edab
Petteri Innanmaa <petteri.innanmaa@gofore.com>
2020-12-15 17:02:53 +0200

    HAI-364: Yet another validations fix

--- src/domain/hanke/edit/validations.ts
+++ src/domain/hanke/edit/validations.ts
@@ -9,12 +9,16 @@ export const isRequiredByFormPage = (formPage: number) => (val: number, schema: 
 export const hankeSchema = yup.object().shape({
   [FORMFIELD.NIMI]: yup.string().required().min(3),
   [FORMFIELD.KUVAUS]: yup.string().required().min(1),
-  [FORMFIELD.ALKU_PVM]: yup.date().required().min(today),
+  [FORMFIELD.ALKU_PVM]: yup.date().nullable().required().min(today),
   [FORMFIELD.LOPPU_PVM]: yup
     .date()
+    .nullable()
     .required()
-    .when(FORMFIELD.ALKU_PVM, (alkuPvm: Date, schema: yup.DateSchema) =>
-      alkuPvm ? schema.min(new Date(alkuPvm)) : schema
+    .when(
+      FORMFIELD.ALKU_PVM,
+      // eslint-disable-next-line
+      // @ts-ignore nullable doesnt work with TS
+      (alkuPvm: Date, schema: yup.DateSchema) => (alkuPvm ? schema.min(new Date(alkuPvm)) : schema)
     ),
   [FORMFIELD.KATUOSOITE]: yup.string().nullable().when('$formPage', isRequiredByFormPage(3)),
 });
