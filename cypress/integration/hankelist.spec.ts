a1980b24a867b78d5adbdc2a91c98b525441a920
Valtteri Laukkanen <1valtteri.laukkanen@gmail.com>
2021-01-14 17:10:45 +0200

    e2e tests failed without this

--- cypress/integration/hankelist.spec.ts
+++ cypress/integration/hankelist.spec.ts
@@ -7,7 +7,6 @@ context('HankeForm', () => {
 
   it('Hanke list testing', () => {
     cy.get('[data-testid=HankeListPageHeader]').should('contain', 'Hankelista');
-    cy.get('[data-testid=toEnd]').click();
     cy.get('[data-testid=toFormLink]').click(); // moves to form
     cy.get('[data-testid=formPageHeader]').should('contain', 'Hanke');
   });
