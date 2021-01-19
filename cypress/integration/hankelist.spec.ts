ae46199667a69fc603d98fff984b5e28fa712478
valger10 <1valtteri.laukkanen@gmail.com>
2021-01-19 16:22:15 +0200

    HAI-457: Tallennus-toast tulee näkyviin siirtymissä (#75)
    * bug fix
    * faking data in e2e tests

--- cypress/integration/hankelist.spec.ts
+++ cypress/integration/hankelist.spec.ts
@@ -1,11 +1,19 @@
 /// <reference types="cypress" />
+// import { initialData } from '../../src/domain/hanke/list/testInitialData';
 
-context('HankeForm', () => {
+context('HankeList', () => {
   beforeEach(() => {
     cy.visit('/fi/hankelista');
   });
 
   it('Hanke list testing', () => {
+    cy.intercept(
+      {
+        method: 'GET', // Route all GET requests
+        url: '/api/hankkeet/', // that have a URL that matches.
+      },
+      [] // and force the response to be: []
+    );
     cy.get('[data-testid=HankeListPageHeader]').should('contain', 'Hankelista');
     cy.get('[data-testid=toFormLink]').click(); // moves to form
     cy.get('[data-testid=formPageHeader]').should('contain', 'Hanke');
