a5b8119bb8242b21c4e6383f52533627de412569
Valtteri Laukkanen <1valtteri.laukkanen@gmail.com>
2021-02-02 14:34:36 +0200

    removed unneccesary tests

--- cypress/integration/hankelist.spec.ts
+++ cypress/integration/hankelist.spec.ts
@@ -1,7 +1,4 @@
 /// <reference types="cypress" />
-
-import initialData from '../../src/domain/hanke/list/e2eInitialData.json';
-
 // import { initialData } from '../../src/domain/hanke/list/testInitialData';
 
 context('HankeList', () => {
@@ -15,15 +12,8 @@ context('HankeList', () => {
         method: 'GET',
         url: '/api/hankkeet/',
       },
-      initialData
+      []
     );
-
-    cy.get('[data-testid=tableHeader0]').click(); // sorts table
-    cy.get('[data-testid=tableHeader1]').click(); // sorts table
-    cy.get('[data-testid=tableHeader2]').click(); // sorts table
-    cy.get('[data-testid=tableHeader3]').click(); // sorts table
-    cy.get('[data-testid=tableHeader4]').click(); // sorts table
-
     cy.get('[data-testid=HankeListPageHeader]').should('contain', 'Hankelista');
     cy.get('[data-testid=toFormLink]').click(); // moves to form
     cy.get('[data-testid=formPageHeader]').should('contain', 'Hanke');
