ca6413bc01628d25d8397e738387af8825c0ac4f
Valtteri Laukkanen <1valtteri.laukkanen@gmail.com>
2021-02-03 13:05:28 +0200

    layout fix

--- src/domain/hanke/list/Table.tsx
+++ src/domain/hanke/list/Table.tsx
@@ -98,15 +98,19 @@ const Table: React.FC<TableProps> = ({ columns, data }) => {
                 {row.cells.map((cell) => {
                   return (
                     <td data-testid={`row${index}_cell_${cell.column.id}`} {...cell.getCellProps()}>
-                      {cell.column.id === 'startDate' || cell.column.id === 'endDate'
-                        ? format(cell.value, 'dd.MM.yyyy')
-                        : cell.render('Cell')}
+                      <span className={cell.column.id}>
+                        {cell.column.id === 'startDate' || cell.column.id === 'endDate'
+                          ? format(cell.value, 'dd.MM.yyyy')
+                          : cell.render('Cell')}
+                      </span>
                     </td>
                   );
                 })}
                 <td>
-                  <IconPen className="pen" />
-                  <IconCrossCircle className="remove" />
+                  <span>
+                    <IconPen className="pen" />
+                    <IconCrossCircle className="remove" />
+                  </span>
                 </td>
               </tr>
             );
