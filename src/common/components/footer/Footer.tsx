2a2031d479bc3f4c6f8029a5004937c72942bd6e
Valtteri Laukkanen <1valtteri.laukkanen@gmail.com>
2021-02-04 14:15:34 +0200

    removed unneccesary code

--- src/common/components/footer/Footer.tsx
+++ src/common/components/footer/Footer.tsx
@@ -6,7 +6,7 @@ const FooterComp: React.FC = () => (
   <footer className="footer" role="contentinfo">
     <div className="footer__wpr" />
     <p>&copy; Copyright 2020 &#8226; All rights reserved</p>
-    <Koros type="basic" aria-label="footer" />
+    <Koros type="basic" />
   </footer>
 );
 
