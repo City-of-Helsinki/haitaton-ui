7627577a8652097405c0a67fdbabb66f9153ba52
Mikko Kujapelto <mikko.kujapelto@gofore.com>
2021-02-12 15:50:16 +0200

    HAI-582 HankeClick interaction implemented;
    - Clicking on the map layer finds geometries from inside VectorLayers
    - On finding the first one it changes the URI query parameter to the found hankeTunnus,
    this should open the sidebar with more detailed information about the hanke

---
+++ src/common/components/map/interactions/HankeClick.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useContext } from 'react';
+import { MapBrowserEvent } from 'ol';
+import OLVectorLayer from 'ol/layer/Vector';
+import { useHistory } from 'react-router-dom';
+import MapContext from '../MapContext';
+import { MapInstance } from '../types';
+
+const HankeClick: React.FC = () => {
+  const { map } = useContext(MapContext);
+
+  const history = useHistory();
+
+  const selectHanke = (mapInstance: MapInstance, evt: MapBrowserEvent<UIEvent>) => {
+    mapInstance?.getLayers().forEach((BaseLayer) => {
+      if (BaseLayer instanceof OLVectorLayer) {
+        BaseLayer.getFeatures(evt.pixel).then((features) => {
+          if (features.length > 0) {
+            features.some((feature) => {
+              const hankeTunnus = feature.get('hankeTunnus');
+              history.push({
+                search: `?hanke=${hankeTunnus}`,
+              });
+              return true;
+            });
+          }
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (map)
+      map.on('click', (evt) => {
+        selectHanke(map, evt);
+      });
+  }, [map]);
+
+  return null;
+};
+
+export default HankeClick;
--- src/domain/map/HankeMap.tsx
+++ src/domain/map/HankeMap.tsx
@@ -16,6 +16,7 @@ import { useMapDataLayers } from './hooks/useMapLayers';
 import { useDateRangeFilter } from './hooks/useDateRangeFilter';
 import { MapTileLayerId } from './types';
 import { HankeData } from '../types/hanke';
+import HankeClick from '../../common/components/map/interactions/HankeClick';
 
 type Props = {
   projectsData: HankeData[];
@@ -78,6 +79,7 @@ const HankeMap: React.FC<Props> = ({ projectsData }) => {
         <Map zoom={zoom} mapClassName={styles.mapContainer__inner}>
           {mapTileLayers.ortokartta.visible && <Ortokartta />}
           {mapTileLayers.kantakartta.visible && <Kantakartta />}
+          <HankeClick />
 
           <VectorLayer
             source={hankeSources.current.features}
