007ee9c0e59a7dbfe5beded37381c871bfffd508
Mikko Kujapelto <52273465+mikko-kujapelto@users.noreply.github.com>
2021-02-16 10:25:13 +0200

    Hai 368 startdate fix and test implementation for dateRangeControl (#109)
    * HAI-652 startDate should begin on 00:00:00 and not 23.59.59
    
    * HAI-648 tests validating dateRangeControl;
    
    - Changing dates affect the number of displayed projects

--- src/common/utils/date.ts
+++ src/common/utils/date.ts
@@ -1,7 +1,10 @@
 import endOfDay from 'date-fns/endOfDay';
+import startOfDay from 'date-fns/startOfDay';
 import format from 'date-fns/format';
 
 // React-datepicker use 00:00 time and TZ so without endOfDay, selected day will be yesterday
 export const toEndOfDayUTCISO = (date: Date) => endOfDay(date).toISOString();
+
+export const toStartOfDayUTCISO = (date: Date) => startOfDay(date).toISOString();
 
 export const formatToFinnishDate = (date: string) => format(new Date(date), 'dd.MM.yyyy');
