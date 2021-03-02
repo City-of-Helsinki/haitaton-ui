b22e450c6ed8491c1f0d4222b592dfec9d269f43
Petteri Innanmaa <petteri.innanmaa@gofore.com>
2021-03-02 12:04:06 +0200

    Remove msw

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
 import App from './common/components/app/App';
 import './locales/i18n';
-// import * as serviceWorker from './serviceWorker';
 
 Sentry.init({
   dsn: process.env.REACT_APP_SENTRY_DSN,
@@ -35,12 +34,6 @@ if (process.env.NODE_ENV !== 'production') {
     document.getElementById('root')
   );
 }
-
-/* if (process.env.NODE_ENV === 'development') {
-  eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { worker } = require('./mocks/browser');
-  worker.start();
-} */
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
