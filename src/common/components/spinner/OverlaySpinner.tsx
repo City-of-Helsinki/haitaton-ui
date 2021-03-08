91f22fbaa497b658c71bedb6b37c1ec80d5ff766
Valtteri Laukkanen <1valtteri.laukkanen@gmail.com>
2021-03-08 14:19:55 +0200

    backup

--- src/common/components/spinner/OverlaySpinner.tsx
+++ src/common/components/spinner/OverlaySpinner.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { LoadingSpinner } from 'hds-react';
+import { Modal, ModalOverlay, useDisclosure } from '@chakra-ui/react';
 
 import styles from './Styles.module.scss';
 
 const OverlaySpinner = () => {
+  const { onClose } = useDisclosure();
+
   return (
-    <>
+    <Modal isOpen onClose={onClose}>
+      <ModalOverlay />
       <div className={styles.spinner}>
-        <div className={styles.spinner__bg} />
-        <div className={styles.spinner__content}>
-          <LoadingSpinner />
-        </div>
+        <LoadingSpinner />
       </div>
-    </>
+    </Modal>
   );
 };
 
