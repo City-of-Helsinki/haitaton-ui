40c75651876803a35683b5101c2759c236ef2a95
Mikko Kujapelto <mikko.kujapelto@gofore.com>
2021-03-09 11:20:40 +0200

    HAI-722 sections created functionally, localization

--- src/domain/map/components/HankeSidebar/HankeIndexes.tsx
+++ src/domain/map/components/HankeSidebar/HankeIndexes.tsx
@@ -1,55 +1,66 @@
 import React from 'react';
 import { StatusLabel } from 'hds-react';
+import { useTranslation } from 'react-i18next';
 import Text from '../../../../common/components/text/Text';
 import styles from './HankeIndexes.module.scss';
+
+type IndexProps = {
+  title: string;
+  content?: string;
+  index: number;
+};
+
+const IndexSection: React.FC<IndexProps> = ({ title, content, index }) =>
+  title && title !== '' && index ? (
+    <>
+      <div className={styles.indexContainer}>
+        <div className={styles.indexContainer__titlesContainer}>
+          <Text tag="h3" styleAs={content ? 'h6' : 'h5'} weight="bold">
+            {title}
+          </Text>
+          {content && (
+            <Text tag="p" styleAs="body-m">
+              {content}
+            </Text>
+          )}
+        </div>
+        <div className={styles.indexContainer__statusContainer}>
+          <StatusLabel type="alert">{index}</StatusLabel>
+        </div>
+      </div>
+    </>
+  ) : null;
 
 type Props = {
   hankeTunnus: string; // FIXME
 };
 
 const HankeIndexes: React.FC<Props> = ({ hankeTunnus }) => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.indexes}>
       <h1>{hankeTunnus}</h1>
-      <div className={styles.indexContainer}>
-        <div className={styles.indexContainer__titlesContainer}>
-          <Text tag="h3" styleAs="h5" weight="bold">
-            Liikennehaittaindeksi
-          </Text>
-        </div>
-        <div
-          className={styles.indexContainer__statusContainer}
-          data-testid="sidebar-liikennehaittaindeksi"
-        >
-          <StatusLabel type="error">4</StatusLabel>
-        </div>
-      </div>
-      <div className={styles.indexContainer}>
-        <div className={styles.indexContainer__titlesContainer}>
-          <Text tag="h3" styleAs="h6" weight="bold">
-            Ruuhkautuminen
-          </Text>
-          <Text tag="p" styleAs="body-m">
-            Kiertoreittitarve: todennäköinen
-          </Text>
-        </div>
-        <div className={styles.indexContainer__statusContainer}>
-          <StatusLabel type="alert">2.7</StatusLabel>
-        </div>
-      </div>
-      <div className={styles.indexContainer}>
-        <div className={styles.indexContainer__titlesContainer}>
-          <Text tag="h3" styleAs="h6" weight="bold">
-            Pyöräilyn pääreitti
-          </Text>
-          <Text tag="p" styleAs="body-m">
-            Kiertoreittitarve: ei tarvetta
-          </Text>
-        </div>
-        <div className={styles.indexContainer__statusContainer}>
-          <StatusLabel type="success">1</StatusLabel>
-        </div>
-      </div>
+
+      <IndexSection title={t('hankeIndexes:liikennehaittaindeksi')} index={4} />
+
+      <IndexSection
+        title={t('hankeIndexes:ruuhkautuminen')}
+        content={`${t('hankeIndexes:kiertoreittitarve')}: ${t('hankeIndexes:todennakoinen')}`}
+        index={2.7}
+      />
+
+      <IndexSection
+        title={t('hankeIndexes:pyorailynPaareitti')}
+        content={`${t('hankeIndexes:kiertoreittitarve')}: ${t('hankeIndexes:eiTarvetta')}`}
+        index={1}
+      />
+
+      <IndexSection
+        title={t('hankeIndexes:merkittavatJoukkoliikennereitit')}
+        content={`${t('hankeIndexes:kiertoreittitarve')}: ${t('hankeIndexes:merkittava')}`}
+        index={4}
+      />
     </div>
   );
 };
