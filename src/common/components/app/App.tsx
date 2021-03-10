954dec7d11059de1db178c7d5bc8486bac4a0559
Petteri Innanmaa <petteri.innanmaa@gofore.com>
2021-03-10 11:43:41 +0200

    HAI-823: Disable query cache

--- src/common/components/app/App.tsx
+++ src/common/components/app/App.tsx
@@ -6,11 +6,16 @@ import { ChakraProvider } from '@chakra-ui/react';
 import AppRoutes from '../../routes/AppRoutes';
 import Layout from './Layout';
 import { store } from '../../redux/store';
-// import theme from './theme';
 import './app.scss';
 import '../../../assets/styles/reset.css';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 0, // Disable cache
+    },
+  },
+});
 
 const App: React.FC = () => (
   <Router>
