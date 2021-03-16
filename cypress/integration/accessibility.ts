975593d65ce740d899743e61d6a9b1afae6bcfa0
Petteri Innanmaa <petteri.innanmaa@gofore.com>
2021-03-16 12:43:31 +0200

    Temporary disabled Axe

--- cypress/integration/accessibility.ts
+++ cypress/integration/accessibility.ts
@@ -11,11 +11,12 @@ context('Accessibility', () => {
 
   it('Hankelist should be accessible', () => {
     cy.visit('/fi/hankelista');
+    // Disabled because of: TypeError: Cannot read property 'run' of undefined in Github actiosn
     // cy.checkA11y();
   });
 
   it('Hankemap should be accessible', () => {
     cy.visit('/fi/map');
-    cy.checkA11y();
+    // cy.checkA11y();
   });
 });
