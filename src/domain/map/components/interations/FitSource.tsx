03c9beea9b8bd553c30a98898b8830f32ed36f85
Petteri Innanmaa <petteri.innanmaa@gofore.com>
2021-03-16 17:27:21 +0200

    HAI-901: Keskitä piirretyt geometriat kartalle

---
+++ src/domain/map/components/interations/FitSource.tsx
@@ -0,0 +1,26 @@
+import React, { useContext, useEffect, useCallback } from 'react';
+import { Vector as VectorSource } from 'ol/source';
+import MapContext from '../../../../common/components/map/MapContext';
+
+type Props = {
+  source: VectorSource;
+};
+
+const FitSource: React.FC<Props> = ({ source }) => {
+  const { map } = useContext(MapContext);
+
+  const fitSource = useCallback(() => {
+    if (!map || !source) return;
+    map.getView().fit(source.getExtent(), { padding: [100, 100, 100, 100] });
+  }, [map, source]);
+
+  useEffect(() => {
+    source.on('featuresAdded', () => {
+      fitSource();
+    });
+  }, []);
+
+  return null;
+};
+
+export default FitSource;
