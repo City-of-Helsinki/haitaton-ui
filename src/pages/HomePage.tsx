b4605c2ffefb3ca79e47de7cdca07c31379bb89a
Petteri <petteri.innanmaa@gofore.com>
2021-03-16 13:02:09 +0200

    HAI-896: Kartta etusivulle (#139)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import Container from '../common/components/container/Container';
+import HankeMapContainer from '../domain/map/HankeMap';
 import PageMeta from './components/PageMeta';
 import { useLocalizedRoutes } from '../common/hooks/useLocalizedRoutes';
-import Text from '../common/components/text/Text';
 
 const HomePage: React.FC = () => {
   const { HOME } = useLocalizedRoutes();
 
   return (
-    <Container>
+    <>
       <PageMeta routeData={HOME} />
-      <Text tag="h1" data-testid="pageHeader" styleAs="h2" spacing="s" weight="bold">
-        Haitaton Beta
-      </Text>
-    </Container>
+      <HankeMapContainer />
+    </>
   );
 };
 
