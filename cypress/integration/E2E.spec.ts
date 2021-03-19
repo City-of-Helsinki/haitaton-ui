a62be098fe59ddb0b1ff78157770989e4630c90d
Mikko Kujapelto <mikko.kujapelto@gofore.com>
2021-03-19 19:25:17 +0200

    HAI-959: implemented dynamic test code for hanke creation
    - When HankeData is provided cypress utils code creates a hanke from that object via UI

--- cypress/integration/E2E.spec.ts
+++ cypress/integration/E2E.spec.ts
@@ -1,4 +1,11 @@
 /// <reference types="cypress" />
+import {
+  HANKE_KAISTAHAITTA,
+  HANKE_KAISTAPITUUSHAITTA,
+  HANKE_MELUHAITTA,
+  HANKE_POLYHAITTA,
+  HANKE_TARINAHAITTA,
+} from './../../src/domain/types/hanke';
 import { createHankeFromUI } from './../utils/formFiller';
 import {
   HANKE_VAIHE,
@@ -6,24 +13,57 @@ import {
   HANKE_SUUNNITTELUVAIHE,
 } from './../../src/domain/types/hanke';
 
-const drawCoordinateX = 100;
-const drawCoordinateY = 100;
-
+// TODO: define types that would not require unnecessary types
 const hankeMock: HankeDataDraft = {
+  id: 0, // not used but types require it
+  hankeTunnus: 'not used', // not used but types require it
+  tilat: {
+    // not used but types require it
+    onGeometrioita: false,
+    onKaikkiPakollisetLuontiTiedot: false,
+    onTiedotLiikenneHaittaIndeksille: false,
+    onLiikenneHaittaIndeksi: false,
+    onViereisiaHankkeita: false,
+    onAsiakasryhmia: false,
+  },
   onYKTHanke: true,
   nimi: 'E2E-hankkeen-testaus',
   kuvaus: 'Tämä on hankkeen kuvaus',
   alkuPvm: '10.01.2030',
   loppuPvm: '11.01.2032',
-  tyomaaKatuosoite: '',
+  haittaAlkuPvm: '10.01.2030',
+  haittaLoppuPvm: '10.01.2032', // TODO: BUG: HAI-974: loppuPvm cant be same as haittaLoppuPvm
+  tyomaaKatuosoite: 'Mannerheimintie 14',
   vaihe: HANKE_VAIHE.SUUNNITTELU,
   suunnitteluVaihe: HANKE_SUUNNITTELUVAIHE.KATUSUUNNITTELU_TAI_ALUEVARAUS,
-  haittaLoppuPvm: new Date('31.12.2031'),
+  omistajat: [
+    {
+      id: null, // not used but types require it
+      etunimi: 'Harri',
+      sukunimi: 'Hankettaja',
+      email: 'harri.hanketest@hankekatu.foo',
+      puhelinnumero: '12341234',
+      organisaatioId: null, // not used but types require it
+      organisaatioNimi: '', // not used but types require it
+      osasto: '', // not used but types require it
+    },
+  ],
+  kaistaHaitta: HANKE_KAISTAHAITTA.VIISI,
+  kaistaPituusHaitta: HANKE_KAISTAPITUUSHAITTA.VIISI,
+  meluHaitta: HANKE_MELUHAITTA.KOLME,
+  polyHaitta: HANKE_POLYHAITTA.KOLME,
+  tarinaHaitta: HANKE_TARINAHAITTA.KOLME,
 };
 
 context('HankeForm', () => {
-  it('Hanke form testing', () => {
+  beforeEach(() => {
     cy.login();
-    createHankeFromUI(hankeMock);
+  });
+
+  it('Hanke form testing', () => {
+    const countIndexes = true;
+    createHankeFromUI(hankeMock, countIndexes);
+    // TODO: validateHankeInfoFromHankeList(hankeMock)
+    // TODO: validateHankeInfoFromMap(hankeMock)
   });
 });
--- cypress/utils/formFiller.ts
+++ cypress/utils/formFiller.ts
@@ -1,6 +1,19 @@
 /// <reference types="cypress" />
 
 import {
+  HANKE_KAISTAHAITTA,
+  HANKE_KAISTAHAITTA_KEY,
+  HANKE_KAISTAPITUUSHAITTA,
+  HANKE_KAISTAPITUUSHAITTA_KEY,
+  HANKE_MELUHAITTA,
+  HANKE_MELUHAITTA_KEY,
+  HANKE_POLYHAITTA,
+  HANKE_POLYHAITTA_KEY,
+  HANKE_TARINAHAITTA,
+  HANKE_TARINAHAITTA_KEY,
+} from './../../src/domain/types/hanke';
+
+import {
   HANKE_VAIHE,
   HankeDataDraft,
   HANKE_SUUNNITTELUVAIHE,
@@ -69,8 +82,185 @@ export const nextFormPage = () => {
   cy.get('[data-testid=forward]').click();
 };
 
-export const createHankeFromUI = (hankeData: HankeDataDraft) => {
+export const drawPolygonToMap = () => {
+  const drawCoordinateX = 100;
+  const drawCoordinateY = 100;
+  cy.get('[data-testid=draw-control-Polygon]').click();
+  cy.get('#ol-map').click(drawCoordinateX, drawCoordinateY);
+  cy.get('#ol-map').click(drawCoordinateX + 300, drawCoordinateY);
+  cy.get('#ol-map').click(drawCoordinateX + 300, drawCoordinateY + 300);
+  cy.get('#ol-map').click(drawCoordinateX, drawCoordinateY + 300);
+  cy.get('#ol-map').dblclick(drawCoordinateX + 20, drawCoordinateY + 20);
+};
+
+export const saveDraft = () => {
+  cy.get('[data-testid=save-draft-button]').click();
+};
+
+export const fillForm2 = (hankeData: HankeDataDraft) => {
+  if (hankeData.omistajat && hankeData.omistajat[0]) {
+    cy.get('input[data-testid=omistajat-etunimi]').type(hankeData.omistajat[0].etunimi);
+    cy.get('input[data-testid=omistajat-sukunimi]').type(hankeData.omistajat[0].sukunimi);
+    cy.get('input[data-testid=omistajat-email]').type(hankeData.omistajat[0].email);
+    cy.get('input[data-testid=omistajat-puhelinnumero]').type(hankeData.omistajat[0].puhelinnumero);
+  }
+};
+
+export const fillForm3 = (hankeData: HankeDataDraft) => {
+  if ('tyomaaKatuosoite' in hankeData && typeof hankeData.tyomaaKatuosoite === 'string') {
+    cy.get('input[data-testid=tyomaaKatuosoite]').type(hankeData.tyomaaKatuosoite);
+  }
+};
+
+export const selectKaistaHaitta = (kaistaHaitta: HANKE_KAISTAHAITTA_KEY) => {
+  cy.get('#kaistaHaitta-toggle-button').click();
+  switch (kaistaHaitta) {
+    case HANKE_KAISTAHAITTA.YKSI:
+      cy.get('#kaistaHaitta-item-0').click();
+      break;
+    case HANKE_KAISTAHAITTA.KAKSI:
+      cy.get('#kaistaHaitta-item-1').click();
+      break;
+    case HANKE_KAISTAHAITTA.KOLME:
+      cy.get('#kaistaHaitta-item-2').click();
+      break;
+    case HANKE_KAISTAHAITTA.NELJA:
+      cy.get('#kaistaHaitta-item-3').click();
+      break;
+    case HANKE_KAISTAHAITTA.VIISI:
+      cy.get('#kaistaHaitta-item-4').click();
+      break;
+    default:
+      break;
+  }
+};
+
+export const selectKaistanPituusHaitta = (kaistanPituusHaitta: HANKE_KAISTAPITUUSHAITTA_KEY) => {
+  cy.get('#kaistaPituusHaitta-toggle-button').click();
+  switch (kaistanPituusHaitta) {
+    case HANKE_KAISTAPITUUSHAITTA.YKSI:
+      cy.get('#kaistaPituusHaitta-item-0').click();
+      break;
+    case HANKE_KAISTAPITUUSHAITTA.KAKSI:
+      cy.get('#kaistaPituusHaitta-item-1').click();
+      break;
+    case HANKE_KAISTAPITUUSHAITTA.KOLME:
+      cy.get('#kaistaPituusHaitta-item-2').click();
+      break;
+    case HANKE_KAISTAPITUUSHAITTA.NELJA:
+      cy.get('#kaistaPituusHaitta-item-3').click();
+      break;
+    case HANKE_KAISTAPITUUSHAITTA.VIISI:
+      cy.get('#kaistaPituusHaitta-item-4').click();
+      break;
+    default:
+      break;
+  }
+};
+
+export const selectMeluHaitta = (meluHaitta: HANKE_MELUHAITTA_KEY) => {
+  cy.get('#meluHaitta-toggle-button').click();
+  switch (meluHaitta) {
+    case HANKE_MELUHAITTA.YKSI:
+      cy.get('#meluHaitta-item-0').click();
+      break;
+    case HANKE_MELUHAITTA.KAKSI:
+      cy.get('#meluHaitta-item-1').click();
+      break;
+    case HANKE_MELUHAITTA.KOLME:
+      cy.get('#meluHaitta-item-2').click();
+      break;
+    default:
+      break;
+  }
+};
+
+export const selectPolyHaitta = (polyHaitta: HANKE_POLYHAITTA_KEY) => {
+  cy.get('#polyHaitta-toggle-button').click();
+  switch (polyHaitta) {
+    case HANKE_POLYHAITTA.YKSI:
+      cy.get('#polyHaitta-item-0').click();
+      break;
+    case HANKE_POLYHAITTA.KAKSI:
+      cy.get('#polyHaitta-item-1').click();
+      break;
+    case HANKE_POLYHAITTA.KOLME:
+      cy.get('#polyHaitta-item-2').click();
+      break;
+    default:
+      break;
+  }
+};
+
+export const selectTarinaHaitta = (tarinaHaitta: HANKE_TARINAHAITTA_KEY) => {
+  cy.get('#tarinaHaitta-toggle-button').click();
+  switch (tarinaHaitta) {
+    case HANKE_TARINAHAITTA.YKSI:
+      cy.get('#tarinaHaitta-item-0').click();
+      break;
+    case HANKE_TARINAHAITTA.KAKSI:
+      cy.get('#tarinaHaitta-item-1').click();
+      break;
+    case HANKE_TARINAHAITTA.KOLME:
+      cy.get('#tarinaHaitta-item-2').click();
+      break;
+    default:
+      break;
+  }
+};
+
+export const fillForm4 = (hankeData: HankeDataDraft) => {
+  typeof hankeData.haittaAlkuPvm === 'string'
+    ? cy.get('#haittaAlkuPvm').type(hankeData.haittaAlkuPvm)
+    : cy.get('#haittaAlkuPvm').type(hankeData.alkuPvm);
+
+  typeof hankeData.haittaLoppuPvm === 'string'
+    ? cy.get('#haittaLoppuPvm').type(hankeData.haittaLoppuPvm)
+    : cy.get('#haittaLoppuPvm').type(hankeData.loppuPvm);
+
+  cy.get('[data-testid=form4Header]').click(); // Close datepicker because it is over kaistaHaitta-toggle-button
+
+  if (typeof hankeData.kaistaHaitta === 'string') {
+    selectKaistaHaitta(hankeData.kaistaHaitta);
+  }
+
+  if (typeof hankeData.kaistaPituusHaitta === 'string') {
+    selectKaistanPituusHaitta(hankeData.kaistaPituusHaitta);
+  }
+
+  if (typeof hankeData.meluHaitta === 'string') {
+    selectMeluHaitta(hankeData.meluHaitta);
+  }
+
+  if (typeof hankeData.polyHaitta === 'string') {
+    selectPolyHaitta(hankeData.polyHaitta);
+  }
+
+  if (typeof hankeData.tarinaHaitta === 'string') {
+    selectTarinaHaitta(hankeData.tarinaHaitta);
+  }
+};
+
+export const countIndexes = () => {
+  // TODO: count indexes implementation here
+};
+
+export const createHankeFromUI = (hankeData: HankeDataDraft, countIndexes: boolean) => {
   cy.visit('/fi/hanke/uusi');
   fillForm0(hankeData);
   nextFormPage();
+  drawPolygonToMap();
+  saveDraft();
+  nextFormPage();
+  fillForm2(hankeData);
+  saveDraft();
+  nextFormPage();
+  fillForm3(hankeData);
+  saveDraft();
+  nextFormPage();
+  fillForm4(hankeData);
+  saveDraft();
+  if (countIndexes) {
+    // TODO: laske indeksit, hyväksy ehdot
+  }
 };
