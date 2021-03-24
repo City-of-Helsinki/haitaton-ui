c590b2f15def72c6f951cdde2bd9932115172925
Petteri Innanmaa <petteri.innanmaa@gofore.com>
2021-03-24 13:16:54 +0200

    HAI-834: Test logout and login with user without haitaton-user-group

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -14,4 +14,19 @@ context('Login', () => {
     cy.visit('/fi/hankelista');
     cy.get('[data-testid=HankeListPageHeader]').should('contain', 'Hankelista');
   });
+
+  it('should logout and login again with user without haitaton-user group', () => {
+    cy.visit('/fi/');
+    cy.get('[data-testid=logoutLink]').click();
+    cy.url().should('include', '/auth/realms/haitaton');
+
+    // Timo does not belong to haitaton-user group. API-calls should fail
+    cy.get('#username').type('timotestaaja@gofore.com');
+    cy.get('#password').type('timo12');
+    cy.get('#kc-login').click();
+
+    cy.visit('/fi/hankelista');
+    // Header exists only when API-request success
+    cy.get('[data-testid=HankeListPageHeader]').should('not.exist');
+  });
 });
