b0b3f6b08d194d2a0077872bb738528c2965e983
Valtteri Laukkanen <1valtteri.laukkanen@gmail.com>
2021-03-25 15:14:08 +0200

    fixed help texts

--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -35,27 +35,32 @@ const Header: React.FC = () => {
         <NavLink to={PROJECTS.path} activeClassName="header--active" data-testid="hankeListLink">
           {PROJECTS.label}
         </NavLink>
-        {isAuthenticated ? (
-          <NavLink
-            to="/logout"
-            activeClassName="header--active"
-            data-testid="loginLink"
-            onClick={(e) => {
-              e.preventDefault();
-              authService.logout();
-            }}
-          >
-            {t('authentication:logoutButton')}
-          </NavLink>
-        ) : (
-          <NavLink to="/login" activeClassName="header--active" data-testid="loginLink">
-            {t('authentication:loginButton')}
-          </NavLink>
-        )}
+
         <NavLink to={NEW_HANKE.path} className="header__hankeLink" data-testid="hankeLink">
           <Locale id="header:hankeLink" />
         </NavLink>
       </Navigation.Row>
+      <Navigation.Actions>
+        <Navigation.Item>
+          {isAuthenticated ? (
+            <NavLink
+              to="/logout"
+              activeClassName="header--active"
+              data-testid="loginLink"
+              onClick={(e) => {
+                e.preventDefault();
+                authService.logout();
+              }}
+            >
+              {t('authentication:logoutButton')}
+            </NavLink>
+          ) : (
+            <NavLink to="/login" activeClassName="header--active" data-testid="loginLink">
+              {t('authentication:loginButton')}
+            </NavLink>
+          )}
+        </Navigation.Item>
+      </Navigation.Actions>
       {/*
       <Navigation.LanguageSelector label={t(`common:languages:${i18n.language}`)}>
         {$enum(LANGUAGES).map((lang) => (
