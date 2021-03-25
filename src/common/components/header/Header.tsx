d2a191fe2f6fe87dda2ae8268024ae42387e6020
Petteri <petteri.innanmaa@gofore.com>
2021-03-25 12:34:42 +0200

    HAI-834: Test logout and login with user without haitaton-user-group (#158)

--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -39,7 +39,7 @@ const Header: React.FC = () => {
           <NavLink
             to="/logout"
             activeClassName="header--active"
-            data-testid="loginLink"
+            data-testid="logoutLink"
             onClick={(e) => {
               e.preventDefault();
               authService.logout();
