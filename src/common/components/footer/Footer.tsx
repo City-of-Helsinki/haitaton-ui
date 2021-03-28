7791e19312957d2d08505902297220cf043e2fee
Petteri Innanmaa <petteri.innanmaa@gofore.com>
2021-03-28 15:03:44 +0300

    Use HDS footer

--- src/common/components/footer/Footer.tsx
+++ src/common/components/footer/Footer.tsx
@@ -1,49 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Koros, Logo } from 'hds-react';
+import { Footer } from 'hds-react';
 import styles from './Footer.module.scss';
 import { useLocalizedRoutes } from '../../hooks/useLocalizedRoutes';
-import Text from '../text/Text';
 
-const FooterComp: React.FC = () => {
+const HaitatonFooter: React.FC = () => {
   const { HAITATON_INFO, ACCESSIBILITY, PRIVACY_POLICY, REFERENCES } = useLocalizedRoutes();
 
   return (
-    <footer className={styles.footer} role="contentinfo">
-      <Koros type="basic" className={styles.koros} />
-      <div className={styles.container}>
-        <div className={styles.footerContent}>
-          <div className={styles.logo}>
-            <div>
-              <Logo size="medium" />
-            </div>
-            <div>
-              <Text tag="h3" weight="bold" styleAs="h6" spacing="2-xs">
-                Haitaton Beta
-              </Text>
-            </div>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <Link to={HAITATON_INFO.path}>{HAITATON_INFO.label}</Link>
-              </li>
-              <li>
-                <Link to={ACCESSIBILITY.path}>{ACCESSIBILITY.label}</Link>
-              </li>
-              <li>
-                <Link to={PRIVACY_POLICY.path}>{PRIVACY_POLICY.label}</Link>
-              </li>
-              <li>
-                <Link to={REFERENCES.path}>{REFERENCES.label}</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <p className={styles.copyRight}>&copy; Copyright 2020 &#8226; All rights reserved</p>
-      </div>
-    </footer>
+    <Footer title="Haitaton Beta" className={styles.footer}>
+      <Footer.Navigation variant="minimal">
+        <Footer.Item>
+          <Link to={HAITATON_INFO.path}>{HAITATON_INFO.label}</Link>
+        </Footer.Item>
+        <Footer.Item>
+          <Link to={ACCESSIBILITY.path}>{ACCESSIBILITY.label}</Link>
+        </Footer.Item>
+        <Footer.Item>
+          <Link to={PRIVACY_POLICY.path}>{PRIVACY_POLICY.label}</Link>
+        </Footer.Item>
+        <Footer.Item>
+          <Link to={REFERENCES.path}>{REFERENCES.label}</Link>
+        </Footer.Item>
+      </Footer.Navigation>
+      <Footer.Base copyrightHolder="Copyright" copyrightText="All rights reserved" />
+    </Footer>
   );
 };
 
-export default FooterComp;
+export default HaitatonFooter;
