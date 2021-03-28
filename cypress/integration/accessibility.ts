d3a82338bc4a490c4f47607f913618cea3c9cbec
Petteri Innanmaa <petteri.innanmaa@gofore.com>
2021-03-28 17:19:24 +0300

    Inject and config axe on each spec

--- cypress/integration/accessibility.ts
+++ cypress/integration/accessibility.ts
@@ -3,24 +3,32 @@
 context('Accessibility', () => {
   beforeEach(() => {
     cy.login();
-    cy.injectAxe();
-    cy.configureAxe({
-      exclude: [['footer']], // False positives on footer color contrast
-    });
   });
 
   it('Hankelist should be accessible', () => {
     cy.visit('/fi/hankelista');
+    cy.injectAxe();
+    cy.configureAxe({
+      exclude: [['footer']],
+    });
     cy.checkA11y();
   });
 
   it('Hankemap should be accessible', () => {
     cy.visit('/fi/map');
+    cy.injectAxe();
+    cy.configureAxe({
+      exclude: [['footer']],
+    });
     cy.checkA11y();
   });
 
   it('Hankeform should be accessible', () => {
     cy.visit('/fi/hanke/uusi');
+    cy.injectAxe();
+    cy.configureAxe({
+      exclude: [['footer']],
+    });
     cy.checkA11y();
   });
 });
