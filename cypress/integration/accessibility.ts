003b29cf23d6bc820c943f564f03664a4c783603
Petteri Innanmaa <petteri.innanmaa@gofore.com>
2021-03-29 11:23:34 +0300

    HAI-1010: Test only static page with Axe

--- cypress/integration/accessibility.ts
+++ cypress/integration/accessibility.ts
@@ -15,9 +15,25 @@ context('Accessibility', () => {
     cy.login();
   });
 
+  /*
+  it('Hankemap should be accessible', () => {
+    cy.visit('/fi/map');
+    cy.injectAxe();
+    cy.configureAxe(axeConfig);
+    cy.checkA11y();
+  }); */
+
+  /* it('Hankeform should be accessible', () => {
+    cy.visit('/fi/hanke/uusi');
+    cy.injectAxe();
+    cy.configureAxe(axeConfig);
+    cy.checkA11y();
+  }); */
+
   it('Saavutettavuusseloste should be accessible', () => {
     cy.visit('/fi/saavutettavuusseloste');
     cy.injectAxe();
     cy.configureAxe(axeConfig);
+    cy.checkA11y();
   });
 });
