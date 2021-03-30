2adf218c4e7ee2ea99c406d82141deb3f4dd9c15
Petteri Innanmaa <petteri.innanmaa@gofore.com>
2021-03-30 10:19:51 +0300

    HAI-1010: Enable Axe tests

--- cypress/integration/accessibility.ts
+++ cypress/integration/accessibility.ts
@@ -15,7 +15,7 @@ context('Accessibility', () => {
     cy.login();
   });
 
-  /* it('Hankelist should be accessible', () => {
+  it('Hankelist should be accessible', () => {
     cy.visit('/fi/hankelista');
     cy.injectAxe();
     cy.configureAxe(axeConfig);
@@ -27,14 +27,14 @@ context('Accessibility', () => {
     cy.injectAxe();
     cy.configureAxe(axeConfig);
     cy.checkA11y();
-  }); */
+  });
 
-  /* it('Hankeform should be accessible', () => {
+  it('Hankeform should be accessible', () => {
     cy.visit('/fi/hanke/uusi');
     cy.injectAxe();
     cy.configureAxe(axeConfig);
     cy.checkA11y();
-  }); */
+  });
 
   it('Saavutettavuusseloste should be accessible', () => {
     cy.visit('/fi/saavutettavuusseloste');
