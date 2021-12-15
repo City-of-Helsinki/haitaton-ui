26c83194820344df6377fd0789cf50b062c6da05
Mikko Kujapelto <mikko.kujapelto@gofore.com>
2021-12-15 16:59:01 +0200

    HAI-1082 update dateRangeControl to use HDS DateInput instead of React datepicker

--- src/common/components/map/controls/DateRangeControl.tsx
+++ src/common/components/map/controls/DateRangeControl.tsx
@@ -1,87 +1,77 @@
 import { GridItem, Grid } from '@chakra-ui/react';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import ReactDatePicker from 'react-datepicker';
+import { DateInput } from 'hds-react';
 import { toStartOfDayUTCISO, toEndOfDayUTCISO, formatToFinnishDate } from '../../../utils/date';
-import CalendarIcon from '../../icons/Calendar';
 import styles from './DateRangeControl.module.scss';
+import useLocale from '../../../hooks/useLocale';
 
 type Props = {
   startDate: string | null;
   updateStartDate: (data: string) => void;
   endDate: string | null;
   updateEndDate: (data: string) => void;
-  isClearable?: boolean;
 };
 
 const startPicker = 'startPicker';
 const endPicker = 'endPicker';
+const ddmmyyyyRegex = /[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{4}/;
 
 const DateRangeControl: React.FC<Props> = ({
   startDate,
   endDate,
   updateStartDate,
   updateEndDate,
-  isClearable,
 }) => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
+  const locale = useLocale();
 
   return (
     <div className={styles.datePicker}>
       <Grid templateColumns="repeat(9, 1fr)" w="360px">
         <GridItem colSpan={4}>
           <div>
-            <label htmlFor={startPicker} className={styles.label}>
-              {t('map:dateRange:begin')}
-            </label>
-            <div className={styles.dateWpr}>
-              <ReactDatePicker
-                id={startPicker}
-                onChange={(date: Date) => {
-                  if (date) {
-                    updateStartDate(toStartOfDayUTCISO(date));
-                  } else {
-                    updateStartDate(date);
-                  }
-                }}
-                value={startDate ? formatToFinnishDate(startDate) : undefined}
-                selected={startDate ? new Date(startDate) : null}
-                maxDate={endDate ? new Date(endDate) : null}
-                locale={i18n.language}
-                isClearable={isClearable}
-              />
-              {!isClearable && <CalendarIcon />}
-            </div>
+            <DateInput
+              id={startPicker}
+              value={startDate ? formatToFinnishDate(startDate) : undefined}
+              initialMonth={new Date()}
+              label={t('map:dateRange:begin')}
+              language={locale}
+              onChange={(e) => {
+                if (e.match(ddmmyyyyRegex)) {
+                  const dateSplit = e.split('.');
+                  const selectedDate = new Date(`${dateSplit[1]}.${dateSplit[0]}.${dateSplit[2]}`);
+                  updateStartDate(toStartOfDayUTCISO(selectedDate));
+                } else {
+                  updateStartDate('');
+                }
+              }}
+              maxDate={endDate ? new Date(endDate) : undefined}
+              disableConfirmation
+            />
           </div>
         </GridItem>
         <GridItem colSpan={1} className={styles.dateHyphen} aria-hidden>
           <p>-</p>
         </GridItem>
         <GridItem colSpan={4}>
-          <div>
-            <label htmlFor={endPicker} className={styles.label}>
-              {t('map:dateRange:end')}
-            </label>
-            <div className={styles.dateWpr}>
-              <ReactDatePicker
-                id={endPicker}
-                onChange={(date: Date) => {
-                  if (date) {
-                    updateEndDate(toEndOfDayUTCISO(date));
-                  } else {
-                    updateEndDate(date);
-                  }
-                }}
-                value={endDate ? formatToFinnishDate(endDate) : undefined}
-                selected={endDate ? new Date(endDate) : null}
-                minDate={startDate ? new Date(startDate) : null}
-                locale={i18n.language}
-                className={styles.reactDatepicker}
-                isClearable={isClearable}
-              />
-              {!isClearable && <CalendarIcon />}
-            </div>
-          </div>
+          <DateInput
+            id={endPicker}
+            value={endDate ? formatToFinnishDate(endDate) : undefined}
+            label={t('map:dateRange:end')}
+            language={locale}
+            onChange={(e) => {
+              if (e.match(ddmmyyyyRegex)) {
+                const dateSplit = e.split('.');
+                const selectedDate = new Date(`${dateSplit[1]}.${dateSplit[0]}.${dateSplit[2]}`);
+                updateEndDate(toEndOfDayUTCISO(selectedDate));
+              } else {
+                updateEndDate('');
+              }
+            }}
+            minDate={startDate ? new Date(startDate) : undefined}
+            disableConfirmation
+          />
         </GridItem>
       </Grid>
     </div>
