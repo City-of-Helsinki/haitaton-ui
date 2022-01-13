73cbdbab9c33b2ef2661e90818e2650792d8a78a
Mikko Kujapelto <mikko.kujapelto@gofore.com>
2022-01-13 15:50:47 +0200

    HAI-1105 add a loading spinner for index numbers when data affecting its calculations is being stored

--- src/domain/hanke/edit/reducer.ts
+++ src/domain/hanke/edit/reducer.ts
@@ -6,6 +6,7 @@ type State = {
   hankeDataDraft: HankeDataFormState;
   hasFormChanged: boolean;
   showNotification: FormNotification;
+  isSaving: boolean;
 };
 
 const updateFormData: CaseReducer<State, PayloadAction<HankeDataFormState>> = (state, action) => {
@@ -27,6 +28,7 @@ export const initialState: State = {
   hankeDataDraft,
   hasFormChanged: false,
   showNotification: null,
+  isSaving: false,
 };
 const formSlice = createSlice({
   name: 'hankeForm',
@@ -38,15 +40,18 @@ const formSlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(saveForm.pending, (state) => {
       state.showNotification = null;
+      state.isSaving = true;
     });
     builder.addCase(saveForm.fulfilled, (state, { payload }) => {
       if (payload) {
         state.hankeDataDraft = payload;
         state.showNotification = 'success';
+        state.isSaving = false;
       }
     });
     builder.addCase(saveForm.rejected, (state) => {
       state.showNotification = 'error';
+      state.isSaving = false;
     });
   },
 });
