eb1efaddb21bc307b37d5678ca12dc09855059f1
Mikko Kujapelto <mikko.kujapelto@gofore.com>
2022-01-14 11:05:50 +0200

    Cleanup: the scope of this unit test is too big. E2E tests validate the same functionality which makes testing this redundant;
    - The test runs +10 seconds on local. In CI it can take 20-40s and often breaks the build.
    Instead of adding more and more time for the test to run, this test is now removed.
    Unit tests of the form should be broken up into smaller unit tests.

--- src/domain/hanke/edit/HankeForm.test.tsx
+++ src/domain/hanke/edit/HankeForm.test.tsx
@@ -13,10 +13,7 @@ jest.setTimeout(10000);
 const nimi = 'test kuoppa';
 const alkuPvm = '24.03.2025';
 const loppuPvm = '25.03.2032';
-const haittaAlkuPvm = '24.03.2025';
-const haittaLoppuPvm = '25.03.2032';
 const omistajaEtunimi = 'Matti';
-const katuosoite = 'Pohjoinen Rautatiekatu 11 b 12';
 const hankeenKuvaus = 'Tässä on kuvaus';
 
 const formData = {
@@ -36,108 +33,6 @@ const formData = {
 };
 
 describe('HankeForm', () => {
-  jest.setTimeout(20000);
-  test('happypath', async () => {
-    const handleSave = jest.fn();
-    const handleSaveGeometry = jest.fn();
-    const handleIsDirtyChange = jest.fn();
-    const handleUnmount = jest.fn();
-    const handleFormClose = jest.fn();
-    const isSaving = false;
-
-    const { getByTestId, getByLabelText, queryAllByText } = render(
-      <HankeForm
-        formData={formData}
-        onSave={handleSave}
-        onSaveGeometry={handleSaveGeometry}
-        onIsDirtyChange={handleIsDirtyChange}
-        onUnmount={handleUnmount}
-        onFormClose={handleFormClose}
-        isSaving={isSaving}
-      />
-    );
-
-    getByTestId(FORMFIELD.YKT_HANKE).click();
-    fireEvent.change(getByTestId(FORMFIELD.NIMI), { target: { value: nimi } });
-    fireEvent.change(getByTestId(FORMFIELD.KUVAUS), { target: { value: hankeenKuvaus } });
-    fireEvent.change(getByLabelText('Hankkeen alkupäivä', { exact: false }), {
-      target: { value: alkuPvm },
-    });
-    fireEvent.change(getByLabelText('Hankkeen loppupäivä', { exact: false }), {
-      target: { value: loppuPvm },
-    });
-    queryAllByText('Hankkeen Vaihe')[0].click();
-    queryAllByText('Ohjelmointi')[0].click();
-    expect(handleIsDirtyChange).toHaveBeenCalledTimes(2);
-    await waitFor(() => expect(getByTestId('forward')).not.toBeDisabled());
-
-    getByTestId('forward').click(); // changes view to form1
-    expect(handleSave).toHaveBeenCalledTimes(1);
-    await waitFor(() => queryAllByText('Hankkeen yhteystiedot')[1]);
-
-    getByTestId('backward').click(); // changes view to form0
-    // await waitFor(() => expect(getByTestId(FORMFIELD.YKT_HANKE)).toBeChecked());
-    expect(getByLabelText('Hankkeen alkupäivä', { exact: false })).toHaveValue(alkuPvm);
-    expect(getByLabelText('Hankkeen loppupäivä', { exact: false })).toHaveValue(loppuPvm);
-    expect(getByTestId(FORMFIELD.NIMI)).toHaveValue(nimi);
-    expect(getByTestId(FORMFIELD.KUVAUS)).toHaveValue(hankeenKuvaus);
-
-    getByTestId('forward').click(); // changes view to form1
-    getByTestId('forward').click(); // changes view to form2
-    await waitFor(() => queryAllByText('Hankkeen yhteystiedot')[1]);
-    expect(handleSave).toHaveBeenCalledTimes(2);
-    fireEvent.change(getByTestId('omistajat-etunimi'), {
-      target: { value: omistajaEtunimi },
-    });
-    fireEvent.change(getByTestId('omistajat-sukunimi'), {
-      target: { value: omistajaEtunimi },
-    });
-    fireEvent.change(getByTestId('omistajat-email'), {
-      target: { value: 'pekka@omistaja.fi' },
-    });
-    fireEvent.change(getByTestId('omistajat-puhelinnumero'), {
-      target: { value: '0406664200' },
-    });
-
-    getByTestId('forward').click(); // changes view to form3
-    await waitFor(() => queryAllByText('Hankkeen lisätiedot')[1]);
-
-    getByTestId('backward').click(); // changes view to form2
-    await waitFor(() => queryAllByText('Hankkeen yhteystiedot')[1]);
-    expect(getByTestId('omistajat-etunimi')).toHaveValue(omistajaEtunimi);
-
-    getByTestId('forward').click(); // changes view to form3
-    await waitFor(() => getByTestId(FORMFIELD.KATUOSOITE));
-    fireEvent.change(getByTestId(FORMFIELD.KATUOSOITE), {
-      target: { value: katuosoite },
-    });
-    await waitFor(() => expect(getByTestId('forward')).not.toBeDisabled());
-
-    getByTestId('forward').click(); // changes view to form4
-    await waitFor(() => queryAllByText('Hankkeen haitat')[1]);
-
-    fireEvent.change(getByLabelText('Haitan alkupäivämäärä', { exact: false }), {
-      target: { value: haittaAlkuPvm },
-    });
-
-    fireEvent.change(getByLabelText('Haitan loppupäivämäärä', { exact: false }), {
-      target: { value: haittaLoppuPvm },
-    });
-
-    getByTestId('backward').click(); // changes view to form3
-    await waitFor(() => getByTestId(FORMFIELD.KATUOSOITE));
-    expect(getByTestId(FORMFIELD.KATUOSOITE)).toHaveValue(katuosoite);
-
-    getByTestId('forward').click(); // changes view to form4
-    await waitFor(() => queryAllByText('Hankkeen haitat')[1]);
-
-    queryAllByText('Kaistahaitta')[0].click();
-    queryAllByText('Ei vaikuta')[0].click();
-
-    queryAllByText('Kaistan pituushaitta')[0].click();
-    queryAllByText('Enintään 10 m')[0].click();
-  });
-
   test('suunnitteluVaihde should be required when vaihe is suunnittelu', async () => {
     const handleSave = jest.fn();
     const handleSaveGeometry = jest.fn();
