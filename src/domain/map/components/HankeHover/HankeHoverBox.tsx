607c076a8f5cdc31f18a852ba347f8d244e2f673
Mikko Kujapelto <mikko.kujapelto@gofore.com>
2022-02-15 13:03:01 +0200

    Hovering over geometry opens a hoverbox next to the geometry;
    Multiple geometries on top of each other are handled

--- src/domain/map/components/HankeHover/HankeHoverBox.tsx
+++ src/domain/map/components/HankeHover/HankeHoverBox.tsx
@@ -1,34 +1,57 @@
 import React, { useContext } from 'react';
 import { format } from 'date-fns';
+import { Link } from 'hds-react';
+import { useNavigate } from 'react-router-dom';
 import HoverContext from '../../../../common/components/map/interactions/hover/HoverContext';
 import HankkeetContext from '../../HankkeetProviderContext';
 import styles from './HankeHover.module.scss';
 import { HankeData } from '../../../types/hanke';
 
 const HankeHoverBox: React.FC = () => {
-  const { hoveredHankeTunnus } = useContext(HoverContext);
+  const { hoveredHankeTunnukset, hoverPosition } = useContext(HoverContext);
   const { hankkeet } = useContext(HankkeetContext);
-  let foundHanke: HankeData | undefined;
+  const foundHankkeet: HankeData[] = [];
 
-  if (hoveredHankeTunnus.length > 0) {
-    foundHanke = hankkeet.find((hanke) => hanke.hankeTunnus === hoveredHankeTunnus);
-  } else {
-    foundHanke = undefined;
+  const navigate = useNavigate();
+
+  const hoverBoxPosition = {
+    top: hoverPosition[1] - 16 || 0,
+    left: hoverPosition[0] + 2 || 0,
+  };
+
+  if (hoveredHankeTunnukset.length > 0) {
+    hoveredHankeTunnukset.forEach((hankeTunnus) => {
+      const hanke = hankkeet.find((HANKE) => HANKE.hankeTunnus === hankeTunnus);
+      if (hanke) {
+        foundHankkeet.push(hanke);
+      }
+    });
   }
 
+  const openHanke = (e: React.MouseEvent, hankeTunnus: string) => {
+    e.preventDefault();
+    navigate({
+      search: `?hanke=${hankeTunnus}`,
+    });
+  };
+
   return (
-    <div className={styles.hankeHover}>
-      {foundHanke && (
+    <div className={styles.hankeHover} style={hoverBoxPosition}>
+      {foundHankkeet.map((hanke) => (
         <div>
+          <Link
+            href={`/?hanke=${hanke.hankeTunnus}`}
+            size="M"
+            onClick={(e) => openHanke(e, hanke.hankeTunnus)}
+          >
+            {`${hanke.nimi} (${hanke.hankeTunnus})`}
+          </Link>
           <p>
-            {foundHanke?.nimi}, ({hoveredHankeTunnus})
-          </p>
-          <p>
-            {format(new Date(foundHanke?.alkuPvm), 'dd.MM.yyyy')} -{' '}
-            {format(new Date(foundHanke?.loppuPvm), 'dd.MM.yyyy')}
+            {format(new Date(hanke.alkuPvm), 'dd.MM.yyyy')} -{' '}
+            {format(new Date(hanke.loppuPvm), 'dd.MM.yyyy')}
           </p>
         </div>
-      )}
+      ))}
     </div>
   );
 };
