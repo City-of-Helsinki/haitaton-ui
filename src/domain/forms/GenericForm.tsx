683dfc06cd333e109b0914edc8fdd0beb6feb816
Patricia Paavilainen <patricia.paavilainen@gofore.com>
2022-02-22 13:54:44 +0200

    HAI-1132 created new generic form base and moved form page indicator to be horizontal

---
+++ src/domain/forms/GenericForm.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Accordion } from 'hds-react';
+import styles from './GenericForm.module.scss';
+import FormPageIndicator from './components/FormPageIndicator';
+import HankeIndexes from '../map/components/HankeSidebar/HankeIndexes';
+import { HankeDataFormState } from '../hanke/edit/types';
+
+type Props = {
+  children: React.ReactNode;
+  formData: HankeDataFormState;
+  isSaving?: boolean;
+};
+
+const GenericForm: React.FC<Props> = ({ formData, isSaving }) => {
+  return (
+    <div className={styles.formWrapper}>
+      <div className={styles.pagination}>
+        <FormPageIndicator currentFormPage={2} />
+      </div>
+      <div className={styles.actions}>lomake action buttonit - poista, keskeytä ja tallenna</div>
+      <Accordion heading="Haittaindeksit" card className={styles.index}>
+        <HankeIndexes
+          hankeIndexData={formData.tormaystarkasteluTulos}
+          displayTooltip
+          loading={isSaving}
+        />
+      </Accordion>
+      <div className={styles.content}>Contettia tänne</div>
+    </div>
+  );
+};
+
+export default GenericForm;
--- src/domain/hanke/edit/HankeFormContainer.tsx
+++ src/domain/hanke/edit/HankeFormContainer.tsx
@@ -15,6 +15,7 @@ import ConfirmationDialog from '../../../common/components/HDSConfirmationDialog
 import api from '../../api/api';
 import { t } from '../../../locales/i18nForTests';
 import { useLocalizedRoutes } from '../../../common/hooks/useLocalizedRoutes';
+import GenericForm from '../../forms/GenericForm';
 
 const getHanke = async (hankeTunnus?: string) => {
   const { data } = await api.get<HankeDataDraft>(`/hankkeet/${hankeTunnus}`);
@@ -109,39 +110,44 @@ const HankeFormContainer: React.FC<Props> = ({ hankeTunnus }) => {
   };
 
   return (
-    <HankeForm
-      formData={formData}
-      onSave={handleSave}
-      onSaveGeometry={handleSaveGeometry}
-      onIsDirtyChange={handleIsDirtyChange}
-      onUnmount={handleUnmount}
-      onFormClose={handleFormClose}
-      isSaving={isSaving}
-      onOpenHankeDelete={openHankeDelete}
-    >
-      <ConfirmationDialog
-        title={t('common:confirmationDialog:deleteDialog:titleText')}
-        description={t('common:confirmationDialog:deleteDialog:bodyText')}
-        isOpen={deleteDialogOpen}
-        close={() => {
-          setDeleteDialogOpen(false);
-          setDeleteErrorMsg('');
-        }}
-        mainAction={() => handleDeleteHanke(formData)}
-        mainBtnLabel={t('common:confirmationDialog:deleteDialog:exitButton')}
-        variant="danger"
-        errorMsg={deleteErrorMsg}
-      />
-      <ConfirmationDialog
-        title={t('common:confirmationDialog:interruptDialog:titleText')}
-        description={t('common:confirmationDialog:interruptDialog:bodyText')}
-        isOpen={interruptDialogOpen}
-        close={() => setInterruptDialogOpen(false)}
-        mainAction={() => closeForm()}
-        mainBtnLabel={t('common:confirmationDialog:interruptDialog:exitButton')}
-        variant="primary"
-      />
-    </HankeForm>
+    <>
+      <GenericForm formData={formData} isSaving={isSaving}>
+        aaaaa
+      </GenericForm>
+      <HankeForm
+        formData={formData}
+        onSave={handleSave}
+        onSaveGeometry={handleSaveGeometry}
+        onIsDirtyChange={handleIsDirtyChange}
+        onUnmount={handleUnmount}
+        onFormClose={handleFormClose}
+        isSaving={isSaving}
+        onOpenHankeDelete={openHankeDelete}
+      >
+        <ConfirmationDialog
+          title={t('common:confirmationDialog:deleteDialog:titleText')}
+          description={t('common:confirmationDialog:deleteDialog:bodyText')}
+          isOpen={deleteDialogOpen}
+          close={() => {
+            setDeleteDialogOpen(false);
+            setDeleteErrorMsg('');
+          }}
+          mainAction={() => handleDeleteHanke(formData)}
+          mainBtnLabel={t('common:confirmationDialog:deleteDialog:exitButton')}
+          variant="danger"
+          errorMsg={deleteErrorMsg}
+        />
+        <ConfirmationDialog
+          title={t('common:confirmationDialog:interruptDialog:titleText')}
+          description={t('common:confirmationDialog:interruptDialog:bodyText')}
+          isOpen={interruptDialogOpen}
+          close={() => setInterruptDialogOpen(false)}
+          mainAction={() => closeForm()}
+          mainBtnLabel={t('common:confirmationDialog:interruptDialog:exitButton')}
+          variant="primary"
+        />
+      </HankeForm>
+    </>
   );
 };
 
