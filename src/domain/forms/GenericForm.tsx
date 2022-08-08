e2c3bd75543d78d43682c6b2c7e022b6bf6e643c
Marko Haarni <marko.haarni@gofore.com>
2022-08-08 14:39:46 +0300

    Add action buttons to form

--- src/domain/forms/GenericForm.tsx
+++ src/domain/forms/GenericForm.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import FormActions from './components/FormActions';
 import styles from './GenericForm.module.scss';
 
 type Props = {
   children: React.ReactNode;
   pagination: React.ReactNode;
+  onDelete: () => void;
+  onClose: () => void;
+  onSave: () => void;
 };
 
-const GenericForm: React.FC<Props> = ({ pagination, children }) => {
+const GenericForm: React.FC<Props> = ({ pagination, children, onDelete, onClose, onSave }) => {
   return (
     <div className={styles.formWrapper}>
       <div className={styles.pagination}>{pagination}</div>
-      <div className={styles.actions}>lomake action buttonit - poista, keskeytä ja tallenna</div>
+      <FormActions onDelete={onDelete} onClose={onClose} onSave={onSave} />
       <div className={styles.content}>{children}</div>
     </div>
   );
