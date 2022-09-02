4453f8551c471ba5b9574e8e5435b7b173de7756
Marko Haarni <marko.haarni@gofore.com>
2022-09-02 10:50:14 +0300

    HAI-1185 Implement new front page for logged in user
    Implemented front page for logged in user based on the new design.

--- src/domain/homepage/HomepageComponent.tsx
+++ src/domain/homepage/HomepageComponent.tsx
@@ -1,126 +1,108 @@
-import {
-  IconDocument,
-  IconInfoCircle,
-  IconMap,
-  IconPlusCircle,
-  Link,
-  Notification,
-} from 'hds-react';
-import React from 'react';
+import React, { useState } from 'react';
+import { Grid } from '@chakra-ui/react';
+import { Koros, Link, Notification } from 'hds-react';
 import { useTranslation } from 'react-i18next';
+import Container from '../../common/components/container/Container';
 import Text from '../../common/components/text/Text';
 import { useLocalizedRoutes } from '../../common/hooks/useLocalizedRoutes';
 import styles from './Homepage.module.scss';
+import img1 from './HKMS000005_km0000pi1k.webp';
+import img2 from './HKMS000005_km0000pi1q.webp';
+import img3 from './HKMS000005_km003yz2.webp';
+import Linkbox from '../../common/components/Linkbox/Linkbox';
 
 const Homepage: React.FC = () => {
   const { t } = useTranslation();
-  const { MAP, PROJECTS, HANKEPORTFOLIO, NEW_HANKE } = useLocalizedRoutes();
-  const Icons = {
-    plus: React.createElement(() => <IconPlusCircle size="s" aria-hidden />),
-    document: React.createElement(() => <IconDocument size="s" aria-hidden />),
-    map: React.createElement(() => <IconMap size="s" aria-hidden />),
-    info: React.createElement(() => <IconInfoCircle size="s" aria-hidden />),
-  };
+  const { MAP, HANKEPORTFOLIO, NEW_HANKE } = useLocalizedRoutes();
+  const [feedbackOpen, setFeedbackOpen] = useState(true);
+
   const basicInfo = [
     {
+      key: 'kartta',
+      actionLink: MAP.path,
+      imgProps: { src: img1, width: 384, height: 245 },
+      external: false,
+    },
+    {
       key: 'hanke',
-      title: 'Hanke',
-      actionIcon: Icons.plus,
       actionLink: NEW_HANKE.path,
+      imgProps: { src: img2, width: 384, height: 245 },
+      external: false,
     },
-    /*
-    {
-      key: 'hakemus',
-      title: 'Hakemus/Työvaihe',
-      actionIcon: Icons.plus,
-      actionLink: '/#',
-    },
-    */
-    {
-      key: 'johtotietoselvitys',
-      title: 'Johtotietoselvitys',
-      actionIcon: Icons.plus,
-      actionLink: '/fi/johtoselvityshakemus',
-    },
-    /*
-    {
-      key: 'valmistelu',
-      title: 'Valmisteluvaiheessa oleva hanke',
-      actionIcon: Icons.plus,
-      actionLink: '/#',
-    },
-    */
     {
       key: 'hankesalkku',
-      title: 'Hankesalkku',
-      actionIcon: Icons.document,
       actionLink: HANKEPORTFOLIO.path,
+      imgProps: { src: img3, width: 384, height: 245 },
+      external: false,
     },
     {
-      key: 'hankelista',
-      title: 'Hankelista',
-      actionIcon: Icons.document,
-      actionLink: PROJECTS.path,
-    },
-    {
-      key: 'kartta',
-      title: 'Kartta',
-      actionIcon: Icons.map,
-      actionLink: MAP.path,
-    },
-    {
-      key: 'tietoja_haitattomasta',
-      title: 'Tietoja Haitattomasta',
-      actionIcon: Icons.info,
-      actionLink: '/fi/tietoja',
+      key: 'ohjeet',
+      actionLink: 'https://tyomaaopas.fi',
+      imgProps: undefined,
+      external: true,
     },
   ];
 
   return (
-    <div className={styles.container}>
-      <div>
-        <Text tag="h1" styleAs="h2" spacing="s" weight="bold">
-          {t('homepage:pageTitle')}
-        </Text>
-        <div className={styles.feedbackInfo}>
-          <Notification
-            label="Auta meitä tekemään Haitattomasta vielä parempi!"
-            type="info"
-            autoClose={false}
-          >
-            <p>
-              Ideoita ja havaintoja voit lähettää osoitteeseen{' '}
-              <a href="mailto:haitaton@hel.fi">haitaton@hel.fi</a>
-            </p>
-          </Notification>
-        </div>
-        <div className={styles.contentContainer}>
-          {basicInfo.map((item) => {
-            return (
-              <div className={styles.basicInfoContainer} key={item.key}>
-                <div>
-                  <Text tag="h2" styleAs="h4" spacing="s" weight="bold">
-                    {t(`homepage:${item.key}:title`)}
-                  </Text>
-                  <Text tag="p" styleAs="body-s" spacing="s">
-                    {t(`homepage:${item.key}:description`)}
-                  </Text>
-                  <Link
-                    className={styles.link}
-                    iconLeft={item.actionIcon}
-                    size="M"
-                    href={item.actionLink}
-                    style={{ display: 'block', width: 'fit-content' }}
-                  >
-                    {t(`homepage:${item.key}:actionText`)}
-                  </Link>
-                </div>
-              </div>
-            );
-          })}
-        </div>
+    <>
+      <div className={styles.heroContainer}>
+        <section className={styles.hero}>
+          <Text tag="h1" styleAs="h1" spacing="s" weight="bold">
+            {t('homepage:pageTitle')}
+          </Text>
+          <Text tag="h2" styleAs="h3" spacing="s" weight="bold">
+            {t('homepage:pageSubTitle')}
+          </Text>
+        </section>
+        <Koros type="basic" flipHorizontal className={styles.koros} />
       </div>
-    </div>
+
+      <Container>
+        <article className={styles.container}>
+          {feedbackOpen && (
+            <div className={styles.feedbackInfo}>
+              <Notification
+                label="Auta meitä tekemään Haitattomasta vielä parempi!"
+                type="info"
+                autoClose={false}
+                dismissible
+                closeButtonLabelText="Close"
+                onClose={() => setFeedbackOpen(false)}
+              >
+                <p>
+                  Ideoita ja havaintoja voit lähettää osoitteeseen
+                  <Link href="mailto:haitaton@hel.fi">haitaton@hel.fi</Link>
+                </p>
+              </Notification>
+            </div>
+          )}
+
+          <Grid
+            templateColumns="repeat(auto-fit, minmax(300px, 1fr))"
+            gap={15}
+            justifyContent="center"
+            alignItems="start"
+          >
+            {basicInfo.map((item) => {
+              return (
+                <div className={styles.linkboxContainer} key={item.key}>
+                  <Linkbox
+                    linkboxAriaLabel={`Linkbox: ${t(`homepage:${item.key}:actionText`)}`}
+                    linkAriaLabel={t(`homepage:${item.key}:actionText`)}
+                    href={item.actionLink}
+                    heading={t(`homepage:${item.key}:title`)}
+                    text={t(`homepage:${item.key}:description`)}
+                    imgProps={item.imgProps}
+                    external={item.external}
+                    openInNewTab={item.external}
+                  />
+                </div>
+              );
+            })}
+          </Grid>
+        </article>
+      </Container>
+    </>
   );
 };
 
