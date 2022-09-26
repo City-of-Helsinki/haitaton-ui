d9092d24ea0b5a6555dcc80857a436ee2b8f6f13
Topias Heinonen <topias.heinonen@gofore.com>
2022-09-26 11:21:34 +0300

    HAI-1139 Improve sentry disabling logic

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ Sentry.init({
   integrations: [new Integrations.BrowserTracing()],
   tracesSampleRate:
     // no traces if not in prod or test
-    window._env_.REACT_APP_DISABLE_SENTRY ? 0.0 : 1.0,
+    window._env_.REACT_APP_DISABLE_SENTRY === '1' ? 0.0 : 1.0,
   environment: process.env.NODE_ENV,
 });
 
