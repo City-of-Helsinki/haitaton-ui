0510b3b282f4750fab28004df26e34894b2bc90c
Marko Haarni <marko.haarni@gofore.com>
2022-10-04 15:11:50 +0300

    HAI-1245 Add tests

--- src/domain/map/components/AddressSearch/AddressSearch.test.tsx
+++ src/domain/map/components/AddressSearch/AddressSearch.test.tsx
@@ -21,3 +21,60 @@ test('Address can be selected from suggestions', async () => {
   userEvent.click(screen.getByText('Elielinaukio 3'));
   expect(handleAddressSelect).toHaveBeenCalledWith([25496700, 6673224]);
 });
+
+test('Swedish address labels are returned when search term is in Swedish', async () => {
+  mockAxios.get.mockResolvedValueOnce({ data: addressData });
+
+  const handleAddressSelect = jest.fn();
+  render(<AddressSearch onAddressSelect={handleAddressSelect} />);
+
+  const searchInput = screen.getByPlaceholderText('Etsi osoitteella');
+
+  userEvent.type(searchInput, 'elielplatsen');
+  await waitFor(() => {
+    expect(mockAxios.get).toHaveBeenCalledTimes(1);
+  });
+
+  expect(screen.getByText('Elielplatsen 1')).toBeInTheDocument();
+  expect(screen.getByText('Elielplatsen 2')).toBeInTheDocument();
+  expect(screen.getByText('Elielplatsen 3')).toBeInTheDocument();
+  expect(screen.getByText('Elielplatsen 5')).toBeInTheDocument();
+});
+
+test('Finnish address labels are returned when search term is in Finnish', async () => {
+  mockAxios.get.mockResolvedValueOnce({ data: addressData });
+
+  const handleAddressSelect = jest.fn();
+  render(<AddressSearch onAddressSelect={handleAddressSelect} />);
+
+  const searchInput = screen.getByPlaceholderText('Etsi osoitteella');
+
+  userEvent.type(searchInput, 'elielinaukio');
+  await waitFor(() => {
+    expect(mockAxios.get).toHaveBeenCalledTimes(1);
+  });
+
+  expect(screen.getByText('Elielinaukio 1')).toBeInTheDocument();
+  expect(screen.getByText('Elielinaukio 2')).toBeInTheDocument();
+  expect(screen.getByText('Elielinaukio 3')).toBeInTheDocument();
+  expect(screen.getByText('Elielinaukio 5')).toBeInTheDocument();
+});
+
+test('Finnish address labels are returned when search term is incomplete and can be either Finnish or Swedish', async () => {
+  mockAxios.get.mockResolvedValueOnce({ data: addressData });
+
+  const handleAddressSelect = jest.fn();
+  render(<AddressSearch onAddressSelect={handleAddressSelect} />);
+
+  const searchInput = screen.getByPlaceholderText('Etsi osoitteella');
+
+  userEvent.type(searchInput, 'eliel');
+  await waitFor(() => {
+    expect(mockAxios.get).toHaveBeenCalledTimes(1);
+  });
+
+  expect(screen.getByText('Elielinaukio 1')).toBeInTheDocument();
+  expect(screen.getByText('Elielinaukio 2')).toBeInTheDocument();
+  expect(screen.getByText('Elielinaukio 3')).toBeInTheDocument();
+  expect(screen.getByText('Elielinaukio 5')).toBeInTheDocument();
+});
