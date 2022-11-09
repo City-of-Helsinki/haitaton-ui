894a0402adb9e1d6b8ffd06efc2180263e7de421
Marko Haarni <marko.haarni@gofore.com>
2022-11-09 16:11:15 +0200

    HAI-1326 Write test for language switcher

--- src/common/components/header/Header.test.tsx
+++ src/common/components/header/Header.test.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { render, cleanup, screen } from '../../../testUtils/render';
 import Header from './Header';
 import useUser from '../../../domain/auth/useUser';
+import i18next from '../../../locales/i18nForTests';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const mockedUseUser = useUser as jest.Mock<any>;
 jest.mock('../../../domain/auth/useUser');
 
@@ -34,5 +37,25 @@ describe('Header', () => {
     render(<Header />);
 
     expect(screen.getAllByText('Test User')).toHaveLength(2);
+  });
+
+  test('when user changes language it should change the UI language and the url based on the selected language', async () => {
+    const user = userEvent.setup();
+    render(<Header />, undefined, '/fi/julkisethankkeet/kartta');
+
+    await user.click(screen.getAllByRole('button', { name: /suomi/i })[0]);
+    await user.click(screen.getAllByText(/english/i)[0]);
+    expect(i18next.language).toBe('en');
+    expect(window.location.pathname).toBe('/en/julkisethankkeet/kartta');
+
+    await user.click(screen.getAllByRole('button', { name: /english/i })[0]);
+    await user.click(screen.getAllByText(/svenska/i)[0]);
+    expect(i18next.language).toBe('sv');
+    expect(window.location.pathname).toBe('/sv/julkisethankkeet/kartta');
+
+    await user.click(screen.getAllByRole('button', { name: /svenska/i })[0]);
+    await user.click(screen.getAllByText(/suomi/i)[0]);
+    expect(i18next.language).toBe('fi');
+    expect(window.location.pathname).toBe('/fi/julkisethankkeet/kartta');
   });
 });
