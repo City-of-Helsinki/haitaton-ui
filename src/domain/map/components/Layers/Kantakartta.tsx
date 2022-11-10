8bd7c6b6db682ebde5cf6870f03066688f3ceab2
Marko Haarni <marko.haarni@gofore.com>
2022-11-10 13:17:17 +0200

    HAI-1254 Change map layer between Opaskartta and Kantakartta when zooming in and out
    Above zoom level 9 display Kantakartta and below level 9 display Opaskartta_Helsinki.

--- src/domain/map/components/Layers/Kantakartta.tsx
+++ src/domain/map/components/Layers/Kantakartta.tsx
@@ -3,30 +3,54 @@ import { TileWMS } from 'ol/source';
 import TileLayer from '../../../../common/components/map/layers/TileLayer';
 import { projection } from '../../../../common/components/map/utils';
 
+const sourceOptions = {
+  url: 'https://kartta.hel.fi/ws/geoserver/avoindata/wms',
+  projection,
+  cacheSize: 1000,
+  imageSmoothing: false,
+  hidpi: false,
+  serverType: 'geoserver',
+  transition: 0,
+  attributions: ['Aineistot &copy; <a href="https://kartta.hel.fi">Helsingin kaupunki</a>'],
+};
+
 const Kantakartta = () => (
-  <TileLayer
-    zIndex={2}
-    source={
-      new TileWMS({
-        url: 'https://kartta.hel.fi/ws/geoserver/avoindata/wms',
-        params: {
-          LAYERS: 'Kantakartta',
-          FORMAT: 'image/jpeg',
-          WIDTH: 256,
-          HEIGHT: 256,
-          VERSION: '1.1.1',
-          TRANSPARENT: 'false',
-        },
-        projection,
-        cacheSize: 1000,
-        imageSmoothing: false,
-        hidpi: false,
-        serverType: 'geoserver',
-        transition: 0,
-        attributions: ['Aineistot &copy; <a href="https://kartta.hel.fi">Helsingin kaupunki</a>'],
-      })
-    }
-  />
+  <>
+    <TileLayer
+      zIndex={2}
+      minZoom={9}
+      source={
+        new TileWMS({
+          ...sourceOptions,
+          params: {
+            LAYERS: 'Kantakartta',
+            FORMAT: 'image/jpeg',
+            WIDTH: 256,
+            HEIGHT: 256,
+            VERSION: '1.1.1',
+            TRANSPARENT: 'false',
+          },
+        })
+      }
+    />
+    <TileLayer
+      zIndex={2}
+      maxZoom={9}
+      source={
+        new TileWMS({
+          ...sourceOptions,
+          params: {
+            LAYERS: 'Opaskartta_Helsinki',
+            FORMAT: 'image/jpeg',
+            WIDTH: 256,
+            HEIGHT: 256,
+            VERSION: '1.1.1',
+            TRANSPARENT: 'false',
+          },
+        })
+      }
+    />
+  </>
 );
 
 export default Kantakartta;
