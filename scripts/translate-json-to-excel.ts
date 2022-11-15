bdcf8598bd5e2702fc626c297a92f0fad4b9701f
nkala <niko.junkala@gofore.com>
2022-11-15 12:24:03 +0200

    HAI-1170 import/export excel

---
+++ scripts/translate-json-to-excel.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env ts-node-script
+
+import * as _ from 'lodash';
+import XLSX from 'xlsx';
+import { LocaleMap, ResultMap, traverse } from './translate-common';
+
+import fi from '../src/locales/fi.json';
+
+function write_file(result: ResultMap) {
+  const worksheet = XLSX.utils.json_to_sheet([..._.values(result)]);
+
+  const book = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(book, worksheet);
+  worksheet['!cols'] = [{ wch: 80 }];
+  XLSX.writeFile(book, 'locale_export.xlsx');
+}
+
+function run() {
+  const result: ResultMap = {};
+  traverse([], fi, result);
+  write_file(result);
+}
+
+run();
