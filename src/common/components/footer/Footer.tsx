0396a8aca6db3d5e7b1871de114d3cb17a14a723
Marko Haarni <marko.haarni@gofore.com>
2023-01-26 15:25:59 +0200

    HAI-1422 Add aria-labels to header and footer nav elements
    Added aria-labels to header and footer nav elements to make them distinct

--- src/common/components/footer/Footer.tsx
+++ src/common/components/footer/Footer.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Footer } from 'hds-react';
+import { useTranslation } from 'react-i18next';
 import { useLocalizedRoutes } from '../../hooks/useLocalizedRoutes';
 
 const HaitatonFooter: React.FC = () => {
+  const { t } = useTranslation();
   const { HAITATON_INFO, ACCESSIBILITY, PRIVACY_POLICY, REFERENCES } = useLocalizedRoutes();
 
   return (
     <Footer title="Haitaton">
-      <Footer.Navigation variant="minimal" navigationAriaLabel="Haitaton tietosivut">
+      <Footer.Navigation
+        variant="minimal"
+        navigationAriaLabel={t('common:ariaLabels:footerNavigation')}
+      >
         <Footer.Item as={Link} to={HAITATON_INFO.path} label={HAITATON_INFO.label} />
         <Footer.Item as={Link} to={ACCESSIBILITY.path} label={ACCESSIBILITY.label} />
         <Footer.Item as={Link} to={PRIVACY_POLICY.path} label={PRIVACY_POLICY.label} />
