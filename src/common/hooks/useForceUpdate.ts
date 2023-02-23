d1ed29f5bb6833ea880d4f325a424339956066e5
Marko Haarni <marko.haarni@gofore.com>
2023-02-23 13:40:29 +0200

    fixup! HAI-1455 Implement naming of cable report application areas

--- src/common/hooks/useForceUpdate.ts
+++ src/common/hooks/useForceUpdate.ts
@@ -1,5 +1,9 @@
 import { useCallback, useState } from 'react';
 
+/**
+ * Returns a forceUpdate function that can be used to
+ * force a React component to re-render
+ */
 export default function useForceUpdate() {
   const [, updateState] = useState(0);
 
