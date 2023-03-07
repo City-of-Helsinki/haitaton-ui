5caa879a4841103c4a247b63d0f4ac811b530ad0
nkala <niko.junkala@gofore.com>
2023-03-07 13:23:21 +0200

    HAI-1170 import/export excel translations (#233)
    * HAI-1170 import/export excel
    
    * Fix target paths
    
    * Fix code smells
    
    * Review note updates
    
    - Remove duplicate line
    - Try fixing line break
    
    * Remove the string replacer

---
+++ scripts/translate-excel-to-json.ts
@@ -0,0 +1,50 @@
+#!/usr/bin/env ts-node-script
+
+import _ from 'lodash';
+import XLSX from 'xlsx';
+import { ResultMap, traverse, flatToDeep } from './translate-common';
+import fs from 'fs';
+
+import fi from '../src/locales/fi.json';
+
+const file_fi = 'src/locales/fi.json';
+const file_sv = 'src/locales/sv.json';
+const file_en = 'src/locales/en.json';
+
+function read_locales() {
+  const file = XLSX.readFile('locale_export.xlsx');
+  const data = XLSX.utils.sheet_to_json(file.Sheets[file.SheetNames[0]]);
+  return _.keyBy(data, 'path');
+}
+
+function write_locales(result: ResultMap) {
+  fs.writeFileSync(file_fi, JSON.stringify(flatToDeep(result, 'fi'), null, 2));
+  fs.writeFileSync(file_sv, JSON.stringify(flatToDeep(result, 'sv'), null, 2));
+  fs.writeFileSync(file_en, JSON.stringify(flatToDeep(result, 'en'), null, 2));
+}
+
+function run() {
+  const original: ResultMap = {};
+  traverse([], fi, original);
+  const newData = read_locales();
+  const combined = _.merge(_.cloneDeep(original), newData);
+  let updated = 0;
+
+  for (const key in combined) {
+    if (!newData[key]) {
+      console.log('Ignoring new key:', key);
+    } else if (!_.isEqual(original[key], combined[key])) {
+      console.log('Updated key "' + key + '"', combined[key]);
+      updated += 1;
+    }
+  }
+
+  if (updated > 0) {
+    write_locales(combined);
+    console.log('Wrote', _.size(combined), 'translation keys');
+  } else {
+    console.log('No changes detected. Doing nothing.');
+  }
+}
+
+run();
