62222fcbe10aa02bd8b7e1af7d959e9331ef7807
Marko Haarni <marko.haarni@gofore.com>
2023-06-14 14:17:10 +0300

    HAI-1753 Fix problem where buttons don't stay in loading state when uploading attachments
    Make sure that previous, next, cancel and save buttons in the bottom stay
    in loading state until all attachments are uploaded.

--- src/domain/johtoselvitys/JohtoselvitysContainer.tsx
+++ src/domain/johtoselvitys/JohtoselvitysContainer.tsx
@@ -477,9 +477,8 @@ const JohtoselvitysContainer: React.FC<Props> = ({ hankeData, application }) => 
           const showSendButton =
             lastStep && isApplicationDraft(getValues('alluStatus') as AlluStatus | null);
 
-          const saveAndQuitIsLoading =
-            applicationSaveMutation.isLoading || attachmentUploadMutation.isLoading;
-          const saveAndQuitLoadingText = attachmentUploadMutation.isLoading
+          const saveAndQuitIsLoading = applicationSaveMutation.isLoading || attachmentsUploading;
+          const saveAndQuitLoadingText = attachmentsUploading
             ? t('form:buttons:loadingAttachments')
             : t('common:buttons:savingText');
           return (
@@ -488,9 +487,9 @@ const JohtoselvitysContainer: React.FC<Props> = ({ hankeData, application }) => 
               totalSteps={formSteps.length}
               onPrevious={handlePreviousPage}
               onNext={handleNextPage}
-              previousButtonIsLoading={attachmentUploadMutation.isLoading}
+              previousButtonIsLoading={attachmentsUploading}
               previousButtonLoadingText={t('form:buttons:loadingAttachments')}
-              nextButtonIsLoading={attachmentUploadMutation.isLoading}
+              nextButtonIsLoading={attachmentsUploading}
               nextButtonLoadingText={t('form:buttons:loadingAttachments')}
             >
               <ApplicationCancel
