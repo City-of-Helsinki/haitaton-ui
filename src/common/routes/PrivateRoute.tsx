740d9940359ed419550556f163499408803b144d
Marko Haarni <marko.haarni@gofore.com>
2023-09-19 12:16:36 +0300

    wip HAI-1311 Redirect user after login

--- src/common/routes/PrivateRoute.tsx
+++ src/common/routes/PrivateRoute.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import useUser from '../../domain/auth/useUser';
+import { REDIRECT_PATH_KEY } from './constants';
 
 type Props = {
   element: JSX.Element;
@@ -8,9 +9,20 @@ type Props = {
 
 const PrivateRoute: React.FC<React.PropsWithChildren<Props>> = ({ element }) => {
   const { data: user } = useUser();
+  const isAuthenticated = Boolean(user?.profile);
 
-  if (!user?.profile) {
-    return <Navigate to="/" />;
+  useEffect(() => {
+    if (isAuthenticated) {
+      sessionStorage.removeItem(REDIRECT_PATH_KEY);
+    }
+  }, [isAuthenticated]);
+
+  if (!isAuthenticated) {
+    // If user tries to access private route when not signed in,
+    // save URL path to session storage and navigate to login,
+    // so that user can be redirected to that route after login
+    sessionStorage.setItem(REDIRECT_PATH_KEY, location.pathname);
+    return <Navigate to="/login" />;
   }
 
   return element;
