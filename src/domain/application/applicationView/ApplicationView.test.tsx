6a749bde72393c2883ffea83f3e36a911fdc987b
Marko Haarni <marko.haarni@gofore.com>
2023-10-02 13:08:01 +0300

    HAI-1947 Show buttons in application view according to user permissions
    Show Edit application and Cancel application buttons if user has
    Kaikki oikeudet, Hankkeen ja hakemuksen muokkaus or Hakemusasiointi
    permission for the hanke.

--- src/domain/application/applicationView/ApplicationView.test.tsx
+++ src/domain/application/applicationView/ApplicationView.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { rest } from 'msw';
-import { render, screen } from '../../../testUtils/render';
+import { render, screen, waitFor } from '../../../testUtils/render';
 import ApplicationViewContainer from './ApplicationViewContainer';
 import { waitForLoadingToFinish } from '../../../testUtils/helperFunctions';
 import { server } from '../../mocks/test-server';
+import { SignedInUser } from '../../hanke/hankeUsers/hankeUser';
 
 test('Correct information about application should be displayed', async () => {
   render(<ApplicationViewContainer id={4} />);
@@ -56,7 +57,7 @@ test('Should show error notification if loading application fails', async () => 
 test('Should be able to go editing application when editing is possible', async () => {
   const { user } = render(<ApplicationViewContainer id={4} />);
 
-  await waitForLoadingToFinish();
+  await waitFor(() => screen.findByRole('button', { name: 'Muokkaa hakemusta' }));
   await user.click(screen.getByRole('button', { name: 'Muokkaa hakemusta' }));
 
   expect(window.location.pathname).toBe('/fi/johtoselvityshakemus/4/muokkaa');
@@ -73,8 +74,7 @@ test('Application edit button should not be displayed when editing is not possib
 test('Should be able to cancel application if it is possible', async () => {
   const { user } = render(<ApplicationViewContainer id={4} />);
 
-  await waitForLoadingToFinish();
-
+  await waitFor(() => screen.findByRole('button', { name: 'Peru hakemus' }));
   await user.click(screen.getByRole('button', { name: 'Peru hakemus' }));
   await user.click(screen.getByRole('button', { name: 'Vahvista' }));
 
@@ -88,5 +88,27 @@ test('Should not be able to cancel application if it has moved to handling in Al
 
   await waitForLoadingToFinish();
 
+  expect(screen.queryByRole('button', { name: 'Peru hakemus' })).not.toBeInTheDocument();
+});
+
+test('Should not show Edit application and Cancel application buttons if user does not have EDIT_APPLICATIONS permission', async () => {
+  server.use(
+    rest.get('/api/hankkeet/:hankeTunnus/whoami', async (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json<SignedInUser>({
+          hankeKayttajaId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+          kayttooikeustaso: 'KATSELUOIKEUS',
+          kayttooikeudet: ['VIEW'],
+        }),
+      );
+    }),
+  );
+
+  render(<ApplicationViewContainer id={4} />);
+
+  await waitForLoadingToFinish();
+
+  expect(screen.queryByRole('button', { name: 'Muokkaa hakemusta' })).not.toBeInTheDocument();
   expect(screen.queryByRole('button', { name: 'Peru hakemus' })).not.toBeInTheDocument();
 });
