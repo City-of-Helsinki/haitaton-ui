732d47c91ff66e35f7a2966c0768be5fae748c2a
Marko Haarni <marko.haarni@gofore.com>
2023-11-09 12:05:02 +0200

    HAI-2067 Add error messages to file upload component (#405)
    Show error message to user for each file that fails to be uploaded,
    either because the file is a duplicate, fails client side validation,
    fails validation in the server or fails because of other server error.
    
    There are three kind of error messages. One for duplicate file,
    one for not passing client side or server side validation and one
    for other server errors.

--- src/common/components/fileUpload/FileUpload.test.tsx
+++ src/common/components/fileUpload/FileUpload.test.tsx
@@ -6,6 +6,7 @@ import FileUpload from './FileUpload';
 import { server } from '../../../domain/mocks/test-server';
 import { AttachmentMetadata } from '../../types/attachment';
 import { deleteAttachment } from '../../../domain/application/attachments';
+import { FileDeleteFunction } from './types';
 
 async function uploadAttachment({ id, file }: { id: number; file: File }) {
   const { data } = await api.post(`/hakemukset/${id}/liitteet`, {
@@ -21,26 +22,46 @@ function uploadFunction(file: File) {
   });
 }
 
-test('Should upload files successfully and loading indicator is displayed', async () => {
-  server.use(
-    rest.post('/api/hakemukset/:id/liitteet', async (req, res, ctx) => {
-      return res(ctx.delay(), ctx.status(200));
-    }),
-  );
+interface FileUploadOptions {
+  accept: string;
+  dragAndDrop: boolean;
+  fileDeleteFunction: FileDeleteFunction;
+  existingAttachments: AttachmentMetadata[];
+}
 
+function getFileUpload(options: Partial<FileUploadOptions> = {}) {
+  const {
+    accept = '.png,.jpg',
+    dragAndDrop,
+    fileDeleteFunction = () => Promise.resolve(),
+    existingAttachments = [],
+  } = options;
   const inputLabel = 'Choose a file';
-  const { user } = render(
+  const renderResult = render(
     <FileUpload
       id="test-file-upload"
       label={inputLabel}
-      accept=".png,.jpg"
+      accept={accept}
       multiple
+      dragAndDrop={dragAndDrop}
       uploadFunction={uploadFunction}
-      fileDeleteFunction={() => Promise.resolve()}
+      fileDeleteFunction={fileDeleteFunction}
+      existingAttachments={existingAttachments}
     />,
+    undefined,
+    undefined,
+    { applyAccept: false },
   );
-  const fileUpload = screen.getByLabelText(inputLabel);
-  user.upload(fileUpload, [
+  const fileUploadElement = screen.getByLabelText(inputLabel);
+  return { renderResult, fileUploadElement };
+}
+
+test('Should upload files successfully and loading indicator is displayed', async () => {
+  const {
+    renderResult: { user },
+    fileUploadElement,
+  } = getFileUpload();
+  user.upload(fileUploadElement, [
     new File(['test-a'], 'test-file-a.png', { type: 'image/png' }),
     new File(['test-b'], 'test-file-b.jpg', { type: 'image/jpg' }),
   ]);
@@ -54,73 +75,114 @@ test('Should upload files successfully and loading indicator is displayed', asyn
   });
 });
 
-test('Should show amount of successful files uploaded correctly when file fails in validation', async () => {
-  const inputLabel = 'Choose a file';
-  const { user } = render(
-    <FileUpload
-      id="test-file-upload"
-      label={inputLabel}
-      accept=".png"
-      multiple
-      uploadFunction={uploadFunction}
-      fileDeleteFunction={() => Promise.resolve()}
-    />,
-    undefined,
-    undefined,
-    { applyAccept: false },
-  );
-  const fileUpload = screen.getByLabelText(inputLabel);
-  user.upload(fileUpload, [
-    new File(['test-a'], 'test-file-a.png', { type: 'image/png' }),
-    new File(['test-b'], 'test-file-b.pdf', { type: 'application/pdf' }),
+test('Should show amount of successful files uploaded and errors correctly when files fails in validation', async () => {
+  const fileNameA = 'test-file-a.png';
+  const fileNameB = 'test-file-b.pdf';
+  const fileNameC = 'test-file-c.png';
+  const fileNameD = 'test-file-d.png';
+  const existingFileA: AttachmentMetadata = {
+    id: '4f08ce3f-a0de-43c6-8ccc-9fe93822ed18',
+    fileName: fileNameA,
+    createdByUserId: 'b9a58f4c-f5fe-11ec-997f-0a580a800284',
+    createdAt: '2023-07-04T12:07:52.324684Z',
+  };
+  const existingFileB: AttachmentMetadata = {
+    id: '4f08ce3f-a0de-43c6-8ccc-9fe93822ed20',
+    fileName: fileNameD,
+    createdByUserId: 'b9a58f4c-f5fe-11ec-997f-0a580a800284',
+    createdAt: '2023-07-04T12:08:52.324684Z',
+  };
+  const {
+    renderResult: { user },
+    fileUploadElement,
+  } = getFileUpload({ accept: '.png', existingAttachments: [existingFileA, existingFileB] });
+  user.upload(fileUploadElement, [
+    new File(['test-a'], fileNameA, { type: 'image/png' }),
+    new File(['test-b'], fileNameB, { type: 'application/pdf' }),
+    new File(['test-c'], fileNameC, { type: 'image/png' }),
   ]);
 
   await waitFor(() => {
-    expect(screen.queryByText('1/2 tiedosto(a) tallennettu')).toBeInTheDocument();
+    expect(screen.queryByText('1/3 tiedosto(a) tallennettu')).toBeInTheDocument();
   });
+  expect(
+    screen.queryByText('Liitteen tallennus epäonnistui 2/3 tiedostolle', { exact: false }),
+  ).toBeInTheDocument();
+  expect(
+    screen.queryByText(
+      `Tiedosto (${fileNameB}) on viallinen. Tarkistathan, että tiedostomuoto on oikea eikä sen koko ylitä sallittua maksimikokoa.`,
+      { exact: false },
+    ),
+  ).toBeInTheDocument();
+  expect(
+    screen.queryByText(
+      `Valittu tiedostonimi (${fileNameA}) on jo käytössä. Nimeä tiedosto uudelleen.`,
+      { exact: false },
+    ),
+  ).toBeInTheDocument();
 });
 
-test('Should show amount of successful files uploaded correctly when request fails', async () => {
+test('Should show amount of successful files uploaded and errors correctly when request fails for bad request', async () => {
   server.use(
     rest.post('/api/hakemukset/:id/liitteet', async (req, res, ctx) => {
       return res(ctx.status(400), ctx.json({ errorMessage: 'Failed for testing purposes' }));
     }),
   );
 
-  const inputLabel = 'Choose a file';
-  const { user } = render(
-    <FileUpload
-      id="test-file-upload"
-      label={inputLabel}
-      accept=".png,.jpg"
-      multiple
-      uploadFunction={uploadFunction}
-      fileDeleteFunction={() => Promise.resolve()}
-    />,
-  );
-  const fileUpload = screen.getByLabelText(inputLabel);
-  user.upload(fileUpload, [new File(['test-a'], 'test-file-a.png', { type: 'image/png' })]);
+  const fileNameA = 'test-file-a.png';
+  const {
+    renderResult: { user },
+    fileUploadElement,
+  } = getFileUpload();
+  user.upload(fileUploadElement, [new File(['test-a'], fileNameA, { type: 'image/png' })]);
 
   await waitFor(() => {
     expect(screen.queryByText('0/1 tiedosto(a) tallennettu')).toBeInTheDocument();
   });
+  expect(
+    screen.queryByText('Liitteen tallennus epäonnistui 1/1 tiedostolle', { exact: false }),
+  ).toBeInTheDocument();
+  expect(
+    screen.queryByText(
+      `Tiedosto (${fileNameA}) on viallinen. Tarkistathan, että tiedostomuoto on oikea eikä sen koko ylitä sallittua maksimikokoa.`,
+      { exact: false },
+    ),
+  ).toBeInTheDocument();
+});
+
+test('Should show correct error message when request fails for server error', async () => {
+  server.use(
+    rest.post('/api/hakemukset/:id/liitteet', async (req, res, ctx) => {
+      return res(ctx.status(500), ctx.json({ errorMessage: 'Failed for testing purposes' }));
+    }),
+  );
+
+  const fileNameA = 'test-file-a.png';
+  const {
+    renderResult: { user },
+    fileUploadElement,
+  } = getFileUpload();
+  user.upload(fileUploadElement, [new File(['test-a'], fileNameA, { type: 'image/png' })]);
+
+  await waitFor(() => {
+    expect(screen.queryByText('0/1 tiedosto(a) tallennettu')).toBeInTheDocument();
+  });
+  expect(
+    screen.queryByText('Liitteen tallennus epäonnistui 1/1 tiedostolle', { exact: false }),
+  ).toBeInTheDocument();
+  expect(
+    screen.queryByText(
+      `Tiedoston (${fileNameA}) lataus epäonnistui, yritä hetken päästä uudelleen.`,
+      { exact: false },
+    ),
+  ).toBeInTheDocument();
 });
 
 test('Should upload files when user drops them into drag-and-drop area', async () => {
-  const inputLabel = 'Choose files';
   const file = new File(['test-file'], 'test-file-a', { type: 'image/png' });
   const file2 = new File(['test-file'], 'test-file-b', { type: 'image/png' });
   const file3 = new File(['test-file'], 'test-file-c', { type: 'image/png' });
-  render(
-    <FileUpload
-      id="test-file-input"
-      label={inputLabel}
-      multiple
-      dragAndDrop
-      uploadFunction={uploadFunction}
-      fileDeleteFunction={() => Promise.resolve()}
-    />,
-  );
+  getFileUpload({ dragAndDrop: true, accept: '' });
   fireEvent.drop(screen.getByText('Raahaa tiedostot tänne'), {
     dataTransfer: {
       files: [file, file2, file3],
@@ -148,17 +210,7 @@ test('Should list added files', async () => {
     createdAt: new Date().toISOString(),
   };
   const files: AttachmentMetadata[] = [fileA, fileB];
-  render(
-    <FileUpload
-      id="test-file-input"
-      label="Choose a file"
-      multiple
-      dragAndDrop
-      uploadFunction={uploadFunction}
-      fileDeleteFunction={() => Promise.resolve()}
-      existingAttachments={files}
-    />,
-  );
+  getFileUpload({ existingAttachments: files });
   const { getAllByRole } = within(screen.getByTestId('file-upload-list'));
   const fileListItems = getAllByRole('listitem');
   expect(fileListItems.length).toBe(2);
@@ -188,17 +240,12 @@ test('Should be able to delete file', async () => {
     createdAt: '2023-09-06T12:09:55.324684Z',
   };
   const files: AttachmentMetadata[] = [fileA, fileB];
-  const { user } = render(
-    <FileUpload
-      id="test-file-input"
-      label="Choose a file"
-      multiple
-      dragAndDrop
-      uploadFunction={uploadFunction}
-      fileDeleteFunction={(file) => deleteAttachment({ applicationId: 1, attachmentId: file?.id })}
-      existingAttachments={files}
-    />,
-  );
+  const {
+    renderResult: { user },
+  } = getFileUpload({
+    existingAttachments: files,
+    fileDeleteFunction: (file) => deleteAttachment({ applicationId: 1, attachmentId: file?.id }),
+  });
   const { getAllByRole } = within(screen.getByTestId('file-upload-list'));
   const fileListItems = getAllByRole('listitem');
   const fileItemA = fileListItems.find((i) => i.innerHTML.includes(fileNameA));
