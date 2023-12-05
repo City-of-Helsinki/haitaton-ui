b802c9d855a76351b76bbf9c885cc2aeb1b2073d
Marko Haarni <marko.haarni@gofore.com>
2023-12-05 13:06:58 +0200

    HAI-2145 Fix changing language redirecting user to front page (#448)
    There was a problem when user was in a route containing dynamic
    path segment such as application id and changed UI language.
    In such case user was taken to front page instead of staying in
    the same page.
    
    Problem was caused because for example key with value of
    'hakemus/4' was not found in language files, so fixed this
    by replacing the id with ':id', so in this case 'hakemus/:id,
    and then placing the actual id back when navigating to changed
    language route.

--- src/common/hooks/useLocalizedRoutes.ts
+++ src/common/hooks/useLocalizedRoutes.ts
@@ -4,6 +4,9 @@ import { $enum } from 'ts-enum-util';
 import { ROUTES, RouteMap } from '../types/route';
 import { Language } from '../types/language';
 
+export const APPLICATION_ID_REGEXP = /(?<=\/)(\d+)/;
+export const HANKETUNNUS_REGEXP = /HAI\d{2}-(\d+)/;
+
 type GetLocalizationParams = {
   useTranslationResponse: UseTranslationResponse<''>;
   route: string;
@@ -28,7 +31,10 @@ export const getRouteLocalization = ({
 export const getMatchingRouteKey = (i18n: i18nInstance, language: Language, path: string) => {
   // eslint-disable-next-line
   const res: any = i18n.getDataByLanguage(language);
-  const pathWithoutLocale = path.length > 3 ? path.substring(3) : path;
+  let pathWithoutLocale = path.length > 3 ? path.substring(3) : path;
+  pathWithoutLocale = pathWithoutLocale
+    .replace(APPLICATION_ID_REGEXP, ':id')
+    .replace(HANKETUNNUS_REGEXP, ':hankeTunnus');
 
   // Find first matching routekey
   const routeKey = Object.keys(res.routes).find((key) =>
