6f273e39229478e2cbe0b8b362afcb923b0b9614
Marko Haarni <marko.haarni@gofore.com>
2024-01-09 09:49:48 +0200

    fixup! HAI-1977 Hide map in hanke list and hanke page if there are no hanke areas

--- src/domain/common/components/hankeInformationView/HankeInformationView.tsx
+++ src/domain/common/components/hankeInformationView/HankeInformationView.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './HankeInformationView.module.scss';
 
-function InformationViewContainer({ children }: { children: React.ReactNode | React.ReactNode[] }) {
+function InformationViewContainer({
+  children,
+}: Readonly<{ children: React.ReactNode | React.ReactNode[] }>) {
   return <article className={styles.informationViewContainer}>{children}</article>;
 }
 
 function InformationViewHeader({
   backgroundColor,
   children,
-}: {
+}: Readonly<{
   backgroundColor: string;
   children: React.ReactNode | React.ReactNode[];
-}) {
+}>) {
   return (
     <header
       style={{ backgroundColor }}
@@ -25,19 +27,19 @@ function InformationViewHeader({
 
 function InformationViewHeaderButtons({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode | React.ReactNode[];
-}) {
+}>) {
   return <div className={styles.buttonContainer}>{children}</div>;
 }
 
 function InformationViewContentContainer({
   children,
   hideSideBar = false,
-}: {
+}: Readonly<{
   children: React.ReactNode | React.ReactNode[];
   hideSideBar?: boolean;
-}) {
+}>) {
   return (
     <div
       className={clsx(styles.contentContainer, {
@@ -52,20 +54,20 @@ function InformationViewContentContainer({
 function InformationViewMainContent({
   children,
   className,
-}: {
+}: Readonly<{
   children: React.ReactNode | React.ReactNode[];
   className?: string;
-}) {
+}>) {
   return <div className={clsx(styles.mainContent, styles.paddingLeft, className)}>{children}</div>;
 }
 
 function InformationViewSidebar({
   children,
   testId,
-}: {
+}: Readonly<{
   children: React.ReactNode | React.ReactNode[];
   testId?: string;
-}) {
+}>) {
   return (
     <div className={styles.sideBar} data-testid={testId}>
       {children}
