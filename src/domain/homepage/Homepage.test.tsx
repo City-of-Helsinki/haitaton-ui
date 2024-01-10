9d859af0d736847991f35f70b2f3a3bca3a75da4
Marko Haarni <marko.haarni@gofore.com>
2024-01-10 13:49:41 +0200

    HAI-2153 Fix user email not being pre-filled in hanke create dialog (#501)
    User email from profile should be pre-filled in hanke create dialog,
    so fixed that.

--- src/domain/homepage/Homepage.test.tsx
+++ src/domain/homepage/Homepage.test.tsx
@@ -6,6 +6,7 @@ import { server } from '../mocks/test-server';
 import Homepage from './HomepageComponent';
 
 const userName = 'Test User';
+const userEmail = 'test.user@mail.com';
 const mockUser: Partial<User> = {
   id_token: 'fffff-aaaaaa-11111',
   access_token: '.GbutWVN1x7RSAP5bU2a-tXdVPuof_9pBNd_Ozw',
@@ -16,6 +17,7 @@ const mockUser: Partial<User> = {
     exp: 0,
     iat: 0,
     name: userName,
+    email: userEmail,
   },
 };
 
@@ -53,6 +55,7 @@ describe('Create new hanke from dialog', () => {
 
   test('Should show validation errors and not create hanke if information is missing', async () => {
     const user = await openHankeCreateDialog();
+    await user.clear(screen.getByLabelText(/sähköposti/i));
     await user.click(screen.getByRole('button', { name: /luo hanke/i }));
 
     expect(screen.getByText(/kentän pituus oltava vähintään 3 merkkiä/i)).toBeInTheDocument();
@@ -72,5 +75,11 @@ describe('Create new hanke from dialog', () => {
 
     expect(screen.getByText('Tapahtui virhe. Yritä uudestaan.')).toBeInTheDocument();
     expect(window.location.pathname).toBe('/');
+  });
+
+  test('Email should be pre-filled', async () => {
+    await openHankeCreateDialog();
+
+    expect(screen.getByLabelText(/sähköposti/i)).toHaveValue(userEmail);
   });
 });
