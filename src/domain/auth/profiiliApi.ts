c0505c024cf438b5fd3c51543ee0308d41d940bf
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2024-02-05 15:12:49 +0200

    HAI-2247 replace original fix with a better solution in useUser hook - remove unnecessarily added explicit return type

--- src/domain/auth/profiiliApi.ts
+++ src/domain/auth/profiiliApi.ts
@@ -1,7 +1,7 @@
 import api from '../api/api';
 import { ProfiiliNimi } from './types';
 
-export async function getProfiiliNimi(): Promise<ProfiiliNimi> {
+export async function getProfiiliNimi() {
   const response = await api.get<ProfiiliNimi>('/profiili/verified-name');
   return response.data;
 }
