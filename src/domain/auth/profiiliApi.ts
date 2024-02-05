3a0ff102ba51fd5de62b5da8f7c53afeae8cb622
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2024-02-05 15:03:38 +0200

    HAI-2247 replace original fix with a better solution in useUser hook

--- src/domain/auth/profiiliApi.ts
+++ src/domain/auth/profiiliApi.ts
@@ -2,12 +2,6 @@ import api from '../api/api';
 import { ProfiiliNimi } from './types';
 
 export async function getProfiiliNimi(): Promise<ProfiiliNimi> {
-  const verifiedName = sessionStorage.getItem('verified-name');
-  if (verifiedName) {
-    return JSON.parse(verifiedName as string);
-  } else {
-    const response = await api.get<ProfiiliNimi>('/profiili/verified-name');
-    sessionStorage.setItem('verified-name', JSON.stringify(response.data));
-    return response.data;
-  }
+  const response = await api.get<ProfiiliNimi>('/profiili/verified-name');
+  return response.data;
 }
--- src/domain/auth/useUser.ts
+++ src/domain/auth/useUser.ts
@@ -2,5 +2,5 @@ import { useQuery } from 'react-query';
 import authService from './authService';
 
 export default function useUser() {
-  return useQuery('user', authService.getUser);
+  return useQuery('user', authService.getUser, { staleTime: Infinity });
 }
