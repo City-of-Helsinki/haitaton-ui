4fed7a802c76ae0bf9fc673f58c61beca5200feb
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2024-02-05 14:49:23 +0200

    HAI-2247 Store verified name into session

--- src/domain/auth/profiiliApi.ts
+++ src/domain/auth/profiiliApi.ts
@@ -1,7 +1,13 @@
 import api from '../api/api';
 import { ProfiiliNimi } from './types';
 
-export async function getProfiiliNimi() {
-  const response = await api.get<ProfiiliNimi>('/profiili/verified-name');
-  return response.data;
+export async function getProfiiliNimi(): Promise<ProfiiliNimi> {
+  const verifiedName = sessionStorage.getItem('verified-name');
+  if (verifiedName) {
+    return JSON.parse(verifiedName as string);
+  } else {
+    const response = await api.get<ProfiiliNimi>('/profiili/verified-name');
+    sessionStorage.setItem('verified-name', JSON.stringify(response.data));
+    return response.data;
+  }
 }
