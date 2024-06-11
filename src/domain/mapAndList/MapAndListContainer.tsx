5c6150c734fbac159e65e3aef93f0e7cc2759b05
Tuomas Alahäivälä <alahaitu@gmail.com>
2024-06-11 15:15:39 +0300

    Change MapAndListContainer "tab" buttons to divs, so the screen reader does not read them twice.

--- src/domain/mapAndList/MapAndListContainer.tsx
+++ src/domain/mapAndList/MapAndListContainer.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Flex } from '@chakra-ui/react';
-import { Button } from 'hds-react';
-import { IconMap, IconLayers } from 'hds-react/icons';
 import { useTranslation } from 'react-i18next';
 import { Outlet, NavLink } from 'react-router-dom';
 import { useLocalizedRoutes } from '../../common/hooks/useLocalizedRoutes';
@@ -25,24 +23,31 @@ const MapAndListContainer: React.FC<React.PropsWithChildren<unknown>> = () => {
         <div>
           <NavLink to={PUBLIC_HANKKEET_MAP.path}>
             {({ isActive }) => (
-              <Button
-                theme="black"
-                variant={isActive ? 'primary' : 'secondary'}
-                iconRight={<IconMap />}
+              <div
+                className={
+                  'hds-button hds-button--theme-black ' +
+                  (isActive ? 'hds-button--primary' : 'hds-button--secondary')
+                }
               >
-                {t('publicHankkeet:buttons:map')}
-              </Button>
+                <span className="hds-button__label">{t('publicHankkeet:buttons:map')}</span>
+                <span className="hds-icon hds-icon--map hds-icon--size-s" aria-hidden="true"></span>
+              </div>
             )}
           </NavLink>
           <NavLink to={PUBLIC_HANKKEET_LIST.path}>
             {({ isActive }) => (
-              <Button
-                theme="black"
-                variant={isActive ? 'primary' : 'secondary'}
-                iconRight={<IconLayers />}
+              <div
+                className={
+                  'hds-button hds-button--theme-black ' +
+                  (isActive ? 'hds-button--primary' : 'hds-button--secondary')
+                }
               >
-                {t('publicHankkeet:buttons:list')}
-              </Button>
+                <span className="hds-button__label">{t('publicHankkeet:buttons:list')}</span>
+                <span
+                  className="hds-icon hds-icon--layers hds-icon--size-s"
+                  aria-hidden="true"
+                ></span>
+              </div>
             )}
           </NavLink>
         </div>
