818e8cd27fd29e96d8349f668986743bda516b8b
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2024-08-14 14:21:34 +0300

    fixup! Support arrays and numbers in locales excel export and import

--- scripts/translate-common.ts
+++ scripts/translate-common.ts
@@ -9,53 +9,33 @@ export type ResultMap = {
   [key: string]: { path: string; fi: string | number; sv: string | number; en: string | number };
 };
 
-export function traverse(path: string[], source: LocaleMap, result: ResultMap) {
-  for (const key in source) {
-    if (_.isString(source[key])) {
-      const mpath = _.join(path, '.') + '.' + key;
-      result[mpath] = {
-        path: mpath,
-        fi: source[key] as string,
-        sv: _.get(sv, mpath),
-        en: _.get(en, mpath),
-      };
-    } else if (_.isNumber(source[key])) {
-      const mpath = _.join(path, '.') + '.' + key;
-      result[mpath] = {
-        path: mpath,
-        fi: source[key] as number,
-        sv: source[key] as number,
-        en: source[key] as number,
-      };
-    } else if (_.isArray(source[key])) {
-      (source[key] as Array<any>).forEach((item, index) => {
-        if (_.isString(item)) {
-          const mpath = _.join(path, '.') + '.' + key + '.' + index.toString();
-          result[mpath] = {
-            path: mpath,
-            fi: item as string,
-            sv: _.get(sv, mpath),
-            en: _.get(en, mpath),
-          };
-        } else if (_.isNumber(item)) {
-          const mpath = _.join(path, '.') + '.' + key + '.' + index.toString();
-          result[mpath] = {
-            path: mpath,
-            fi: item,
-            sv: item,
-            en: item,
-          };
-        } else if (_.isObject(item)) {
-          traverse([...path, key, index.toString()], item, result);
-        } else {
-          throw new Error('Unkown type of ');
-        }
-      });
-    } else if (_.isObject(source[key])) {
-      traverse([...path, key], source[key] as LocaleMap, result);
-    } else {
-      throw new Error('Unkown type of ');
+export function traverse(path: string[], source: any, result: ResultMap) {
+  if (_.isString(source)) {
+    const mpath = _.join(path, '.');
+    result[mpath] = {
+      path: mpath,
+      fi: source as string,
+      sv: _.get(sv, mpath),
+      en: _.get(en, mpath),
+    };
+  } else if (_.isNumber(source)) {
+    const mpath = _.join(path, '.');
+    result[mpath] = {
+      path: mpath,
+      fi: source as number,
+      sv: source as number,
+      en: source as number,
+    };
+  } else if (_.isArray(source)) {
+    (source as Array<any>).forEach((item, index) => {
+      traverse([...path, index.toString()], item, result);
+    });
+  } else if (_.isObject(source)) {
+    for (const key in source) {
+      traverse([...path, key], (source as LocaleMap)[key], result);
     }
+  } else {
+    throw new Error('Unknown type of ');
   }
 }
 
