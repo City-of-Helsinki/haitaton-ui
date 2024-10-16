7bce07daa36d76a4d4e77ad8d12a1ad807884ac7
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2024-10-16 13:39:39 +0300

    HAI-2756 Add missing 'required' for work purpose selection (#927)

--- src/domain/kaivuilmoitus/Areas.tsx
+++ src/domain/kaivuilmoitus/Areas.tsx
@@ -451,6 +451,7 @@ export default function Areas({ hankeData }: Readonly<Props>) {
                       label={t('hakemus:labels:tyonTarkoitus')}
                       mapValueToLabel={(value) => t(`hanke:tyomaaTyyppi:${value}`)}
                       errorMsg={t('hankeForm:insertFieldError')}
+                      required
                     />
                   </Box>
 
