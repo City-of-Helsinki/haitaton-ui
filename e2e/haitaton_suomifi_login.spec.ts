8563d9f1a0c0160b6429b90ea2f8bbba6be6edd7
Topias Heinonen <topias.heinonen@gofore.com>
2025-02-19 12:45:06 +0200

    Add a random output to make sure the branch is actually active

--- e2e/haitaton_suomifi_login.spec.ts
+++ e2e/haitaton_suomifi_login.spec.ts
@@ -1,6 +1,8 @@
 import { test } from '@playwright/test';
 import { helsinkiLogin } from './_setup';
 
+console.log('In delete report directory branch.');
+
 test('Johtoselvityshakemus ei ole käytettävissä ennen kirjautumista', async ({ page }) => {
   await helsinkiLogin(page);
 });
