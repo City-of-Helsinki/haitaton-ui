d6b99791a3aa945a00656c3f83f63f16d2c37a0d
Marko Haarni <marko.haarni@gofore.com>
2025-03-05 16:01:02 +0200

    fixup! HAI-3405 Show muutosilmoitus changes in application view

--- src/domain/application/taydennysAndMuutosilmoitusCommon/MuutosLabelContext.tsx
+++ src/domain/application/taydennysAndMuutosilmoitusCommon/MuutosLabelContext.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { createContext, useContext } from 'react';
+import React, { createContext, useContext } from 'react';
 
 const MuutosLabelContext = createContext<string | undefined>(undefined);
 
 export function MuutosLabelProvider({
   children,
   value,
-}: {
+}: Readonly<{
   children: React.ReactNode;
   value: string | undefined;
-}) {
+}>) {
   return <MuutosLabelContext.Provider value={value}>{children}</MuutosLabelContext.Provider>;
 }
 
