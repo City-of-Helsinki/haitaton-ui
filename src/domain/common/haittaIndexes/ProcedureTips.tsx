7d01e9499484c3affea35fc4818e4f070f6b04ef
Tuomas Alahäivälä <alahaitu@gmail.com>
2025-03-18 09:38:42 +0200

    Fix icons not showing and other template mishaps

--- src/domain/common/haittaIndexes/ProcedureTips.tsx
+++ src/domain/common/haittaIndexes/ProcedureTips.tsx
@@ -21,8 +21,6 @@ export default function ProcedureTips({ haittojenhallintaTyyppi, haittaIndex }: 
     tip: string;
     // Index treshold for showing the tip (if undefined, show the tip always)
     indexTreshold?: number;
-    // List of possible link hrefs to be used in the tip text
-    links?: string[];
     // Links to procedure tip cards
     cardLinks?: number[];
   }[] = t(`hankeForm:haittojenHallintaForm:procedureTips:${haittojenhallintaTyyppi}`, {
@@ -48,16 +46,13 @@ export default function ProcedureTips({ haittojenhallintaTyyppi, haittaIndex }: 
     external: (
       <a
         className="hds-link hds-link--medium"
-        aria-label={t('common:components:link:openInNewTabAriaLabel')}
+        aria-label={`${t('common:components:link:openInNewTabAriaLabel')} ${t('common:components:link:openInExternalDomainAriaLabel')}`}
         target="_blank"
       >
         External link
-        <span
-          className="hds-icon icon hds-icon--link-external hds-icon--size-s vertical-align-medium-icon"
-          aria-hidden="true"
-        ></span>
       </a>
     ),
+    span: <span />,
     mailto: (
       <a
         className="hds-link hds-link--medium"
@@ -70,63 +65,61 @@ export default function ProcedureTips({ haittojenhallintaTyyppi, haittaIndex }: 
   };
 
   return (
-    <>
-      <Box
-        paddingX="var(--spacing-s)"
-        paddingBottom="var(--spacing-s)"
-        mb={isOther ? 'var(--spacing-xs)' : 'var(--spacing-l)'}
-        backgroundColor="var(--color-black-5)"
-        data-testid={`show-tips-button-${haittojenhallintaTyyppi}`}
+    <Box
+      paddingX="var(--spacing-s)"
+      paddingBottom="var(--spacing-s)"
+      mb={isOther ? 'var(--spacing-xs)' : 'var(--spacing-l)'}
+      backgroundColor="var(--color-black-5)"
+      data-testid={`show-tips-button-${haittojenhallintaTyyppi}`}
+    >
+      <Accordion
+        heading={t(
+          `hankeForm:haittojenHallintaForm:procedureTips:titles:${haittojenhallintaTyyppi}`,
+        )}
+        size="s"
+        headingLevel={4}
+        theme={{
+          '--header-font-size': 'var(--fontsize-heading-s)',
+        }}
       >
-        <Accordion
-          heading={t(
-            `hankeForm:haittojenHallintaForm:procedureTips:titles:${haittojenhallintaTyyppi}`,
-          )}
-          size="s"
-          headingLevel={4}
-          theme={{
-            '--header-font-size': 'var(--fontsize-heading-s)',
-          }}
-        >
-          <Box mb="var(--spacing-s)">
-            {filteredTips.map((item) => (
-              <Accordion
-                key={item.heading}
-                heading={item.heading}
-                size="s"
-                headingLevel={5}
-                theme={{
-                  '--content-font-size': 'var(--fontsize-body-m)',
-                  '--header-font-size': 'var(--fontsize-heading-xs)',
-                }}
-              >
+        <Box mb="var(--spacing-s)">
+          {filteredTips.map((item) => (
+            <Accordion
+              key={item.heading}
+              heading={item.heading}
+              size="s"
+              headingLevel={5}
+              theme={{
+                '--content-font-size': 'var(--fontsize-body-m)',
+                '--header-font-size': 'var(--fontsize-heading-xs)',
+              }}
+            >
+              <Box as="p" mb="var(--spacing-s)">
+                <Trans i18nKey={item.tip} components={translationComponents} />
+              </Box>
+              {item.cardLinks && item.cardLinks.length > 0 && (
                 <Box as="p" mb="var(--spacing-s)">
-                  <Trans i18nKey={item.tip} components={translationComponents} />
+                  <strong>
+                    {t('hankeForm:haittojenHallintaForm:procedureTips:common:nuisanceCardLinks')}
+                  </strong>
+                  {item.cardLinks?.map((linkId) => (
+                    <Link
+                      key={linkId}
+                      iconLeft={<IconDocument />}
+                      size="M"
+                      href={`${CARD.path}${linkId}/${t('routes:CARD:basicLevel')}`}
+                      openInNewTab
+                      openInNewTabAriaLabel={t('common:components:link:openInNewTabAriaLabel')}
+                    >
+                      {t(`workInstructions:cards:${linkId}:header`)}
+                    </Link>
+                  ))}
                 </Box>
-                {item.cardLinks && item.cardLinks.length > 0 && (
-                  <Box as="p" mb="var(--spacing-s)">
-                    <strong>
-                      {t('hankeForm:haittojenHallintaForm:procedureTips:common:nuisanceCardLinks')}
-                    </strong>
-                    {item.cardLinks?.map((linkId) => (
-                      <Link
-                        key={linkId}
-                        iconLeft={<IconDocument />}
-                        size="M"
-                        href={`${CARD.path}${linkId}/${t('routes:CARD:basicLevel')}`}
-                        openInNewTab
-                        openInNewTabAriaLabel={t('common:components:link:openInNewTabAriaLabel')}
-                      >
-                        {t(`workInstructions:cards:${linkId}:header`)}
-                      </Link>
-                    ))}
-                  </Box>
-                )}
-              </Accordion>
-            ))}
-          </Box>
-        </Accordion>
-      </Box>
-    </>
+              )}
+            </Accordion>
+          ))}
+        </Box>
+      </Accordion>
+    </Box>
   );
 }
