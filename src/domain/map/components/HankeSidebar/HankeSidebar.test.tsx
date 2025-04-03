476b9fcf5da0f4b1e3639d77a146388d93989405
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2025-04-03 11:47:04 +0300

    fixup! HAI-3421 Make public hanke info view to follow the new specification

--- src/domain/map/components/HankeSidebar/HankeSidebar.test.tsx
+++ src/domain/map/components/HankeSidebar/HankeSidebar.test.tsx
@@ -1,23 +1,30 @@
 import HankeSidebar from './HankeSidebar';
 import hankeList from '../../../mocks/hankeList';
 import { cleanup, render, screen } from '../../../../testUtils/render';
+import { formatToFinnishDate } from '../../../../common/utils/date';
 
 afterEach(cleanup);
 
 describe('HankeSidebar', () => {
   test('Should be display data correctly', async () => {
+    const hanke = hankeList[0];
+    const hankealue = hanke.alueet[0];
     render(
       <HankeSidebar
-        hanke={hankeList[0]}
-        hankealueNimi={hankeList[0].alueet[0].nimi!}
+        hanke={hanke}
+        hankealueNimi={hankealue.nimi!}
         isOpen
         handleClose={() => ({})}
       />,
     );
+    const hankealueAlkuPvm = formatToFinnishDate(hankealue.haittaAlkuPvm);
+    const hankealueLoppuPvm = formatToFinnishDate(hankealue.haittaLoppuPvm);
+    const hankeAlkuPvm = formatToFinnishDate(hanke.alkuPvm);
+    const hankeLoppuPvm = formatToFinnishDate(hanke.loppuPvm);
     expect(screen.getByText('Mannerheimintie autottomaksi: Hankealue 1')).toBeInTheDocument();
-    expect(screen.getByText('3.10.2022 - 9.10.2022')).toBeInTheDocument();
+    expect(screen.getByText(`${hankealueAlkuPvm} - ${hankealueLoppuPvm}`)).toBeInTheDocument();
     expect(screen.getByText('Mannerheimintie autottomaksi (HAI22-1)')).toBeInTheDocument();
-    expect(screen.getByText('2.10.2022 - 10.10.2022')).toBeInTheDocument();
+    expect(screen.getByText(`${hankeAlkuPvm} - ${hankeLoppuPvm}`)).toBeInTheDocument();
     expect(screen.getByText('Yksityishenkilö')).toBeInTheDocument();
     expect(screen.getByText('Ohjelmointi')).toBeInTheDocument();
     expect(screen.getByText('Vesi, Viemäri')).toBeInTheDocument();
