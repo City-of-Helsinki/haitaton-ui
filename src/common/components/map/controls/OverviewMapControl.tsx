6176ddce9c1ba00a77e1daba02919a9acbdaab38
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2025-05-11 10:16:38 +0300

    HAI-3568 Try fixing E2E test failure of OverviewMap (#1217)

--- src/common/components/map/controls/OverviewMapControl.tsx
+++ src/common/components/map/controls/OverviewMapControl.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import OLTileLayer from 'ol/layer/Tile';
 import { TileWMS } from 'ol/source';
 import { MapBrowserEvent, View } from 'ol';
@@ -25,53 +25,69 @@ const OverviewMapControl: React.FC<React.PropsWithChildren<Props>> = ({ classNam
   useEffect(() => {
     if (!map) return;
 
-    const overviewMapTileLayer = new OLTileLayer({
-      source: new TileWMS({
-        url: 'https://kartta.hel.fi/ws/geoserver/avoindata/wms',
-        params: {
-          LAYERS: 'Opaskartta_Helsinki',
-          FORMAT: 'image/jpeg',
-          WIDTH: 256,
-          HEIGHT: 256,
-          VERSION: '1.1.1',
-          TRANSPARENT: 'false',
-        },
-        projection: projection || undefined,
-        cacheSize: 1000,
-        hidpi: false,
-        serverType: 'geoserver',
-        transition: 0,
-      }),
-    });
-
-    const overviewMapControl = new OverviewMap({
-      className: clsx(['ol-overviewmap', 'ol-custom-overviewmap', className]),
-      layers: [overviewMapTileLayer],
-      collapseLabel: '\u00BB',
-      label: '\u00AB',
-      collapsed: false,
-      view: new View({
-        zoom: 3,
-        minZoom: 2,
-        maxZoom: 6,
-        projection: projection || undefined,
-      }),
-    });
-
-    map.addControl(overviewMapControl);
-    map.addInteraction(new DragZoom());
+    let overviewCtrl: OverviewMap;
+    let dragZoom: DragZoom;
 
     // Add possibility to move map by clicking a point
     // on overview map
     function overviewMapClick(e: MapBrowserEvent<UIEvent>) {
       map?.getView().setCenter(e.coordinate);
     }
-    overviewMapControl.getOverviewMap().on('click', overviewMapClick);
 
-    // eslint-disable-next-line
-    return function cleanup() {
-      overviewMapControl.getOverviewMap().un('click', overviewMapClick);
-      map.removeControl(overviewMapControl);
+    const initOverview = () => {
+      const overviewMapTileLayer = new OLTileLayer({
+        source: new TileWMS({
+          url: 'https://kartta.hel.fi/ws/geoserver/avoindata/wms',
+          params: {
+            LAYERS: 'Opaskartta_Helsinki',
+            FORMAT: 'image/jpeg',
+            WIDTH: 256,
+            HEIGHT: 256,
+            VERSION: '1.1.1',
+            TRANSPARENT: 'false',
+          },
+          projection: projection || undefined,
+          cacheSize: 1000,
+          hidpi: false,
+          serverType: 'geoserver',
+          transition: 0,
+        }),
+      });
+
+      const overviewMapControl = new OverviewMap({
+        className: clsx(['ol-overviewmap', 'ol-custom-overviewmap', className]),
+        layers: [overviewMapTileLayer],
+        collapseLabel: '\u00BB',
+        label: '\u00AB',
+        collapsed: false,
+        view: new View({
+          zoom: 3,
+          minZoom: 2,
+          maxZoom: 6,
+          projection: projection || undefined,
+        }),
+      });
+
+      map.addControl(overviewMapControl);
+      dragZoom = new DragZoom();
+      map.addInteraction(dragZoom);
+
+      const overviewMap = overviewMapControl.getOverviewMap();
+      overviewMap.on('click', overviewMapClick);
+    };
+
+    map.once('rendercomplete', initOverview);
+
+    return () => {
+      if (overviewCtrl) {
+        const ov = overviewCtrl.getOverviewMap();
+        ov.un('click', overviewMapClick);
+        ov.dispose();
+        map.removeControl(overviewCtrl);
+      }
+      if (dragZoom) {
+        map.removeInteraction(dragZoom);
+      }
     };
   }, [map, className]);
 
