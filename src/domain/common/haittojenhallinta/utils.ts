4c02b424f6af18bb6fe1ccfa6d1a6da8c6b78083
Petteri Kujala <petteri.kujala@gofore.com>
2025-08-27 17:25:19 +0300

    HAI-3567 fix linting issues

--- src/domain/common/haittojenhallinta/utils.ts
+++ src/domain/common/haittojenhallinta/utils.ts
@@ -35,8 +35,10 @@ export function sortedLiikenneHaittojenhallintatyyppi(
     .sort((a, b): number => {
       const diff = b.value - a.value;
       if (diff === 0) {
-        // @ts-expect-error Haittatyyppi comparison does not look good for TSLinting
-        return defaultOrder.indexOf(a.type) - defaultOrder.indexOf(b.type);
+        return (
+          defaultOrder.indexOf(a.type as (typeof defaultOrder)[number]) -
+          defaultOrder.indexOf(b.type as (typeof defaultOrder)[number])
+        );
       }
       return diff;
     });
