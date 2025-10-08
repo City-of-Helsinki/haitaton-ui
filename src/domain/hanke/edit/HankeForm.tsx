f76082aecd0ea3e62884b56d66dcbd59ae95d27e
Satu Kautto <115613569+SatuKautt@users.noreply.github.com>
2025-10-08 14:33:12 +0300

    change project save error in haittojenhallinta (#1287)
    * change project save error in haittojenhallinta
    
    * adding tests for HankeForm
    
    * add more tests

--- src/domain/hanke/edit/HankeForm.tsx
+++ src/domain/hanke/edit/HankeForm.tsx
@@ -82,6 +82,7 @@ const HankeForm: React.FC<React.PropsWithChildren<Props>> = ({
     setValue,
     trigger,
     watch,
+    setFocus,
   } = formContext;
 
   const formValues = getValues();
@@ -254,6 +255,17 @@ const HankeForm: React.FC<React.PropsWithChildren<Props>> = ({
         closeButtonLabelText: t('common:components:notification:closeButtonLabelText'),
       });
       setActiveStepIndex(2);
+      return;
+    }
+// If hanke is public and there are missing fields dont save, focus on the first missing field
+    if (isHankePublic && haittojenHallintaErrors.length > 0) {
+      const firstErrorField = haittojenHallintaErrors[0]?.path;
+      setActiveStepIndex(3);
+      if (typeof firstErrorField === 'string') {
+        setTimeout(() => {
+          setFocus(firstErrorField as FieldPath<HankeDataFormState>);
+        }, 100);
+      }
       return;
     }
 
